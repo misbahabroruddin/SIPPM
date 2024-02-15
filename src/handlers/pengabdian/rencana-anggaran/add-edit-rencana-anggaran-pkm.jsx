ca01bfd512f266bf813aae164f3f0c1e3618bd47
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-15 16:25:05 +0700

    feat: add handler for add/edit rencana anggaran pkm

---
+++ src/handlers/pengabdian/rencana-anggaran/add-edit-rencana-anggaran-pkm.jsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+
+export const useAddEditRencanaAnggaranPKM = (id, reset, onClose) => {
+  const axios = useAxios();
+  const queryClient = useQueryClient();
+  const onSubmit = async (form) => {
+    try {
+      const pengabdianId = localStorage.getItem("pengabdianId");
+      const formData = new FormData();
+      formData.append("rincian", form.rincian);
+      formData.append("biaya", form.biaya);
+      if (id) {
+        const { data } = await axios.put(
+          `/proposals/dosen/pkms/${pengabdianId}/rencana-anggarans/${id}`,
+          {
+            rincian: form.rincian,
+            biaya: form.biaya,
+          },
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        );
+        toast.success("Rencana anggaran PKM berhasil diubah");
+        return data;
+      } else {
+        const { data } = await axios.post(
+          `/proposals/dosen/pkms/${pengabdianId}/rencana-anggarans`,
+          formData
+        );
+        reset();
+        toast.success("Rencana anggaran PKM berhasil ditambah");
+        return data;
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const { mutateAsync: addEditRencanaAnggaran, isPending } = useMutation({
+    mutationFn: onSubmit,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["rencanaAnggaranPKM"] });
+      onClose();
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return {
+    addEditRencanaAnggaran,
+    isPending,
+  };
+};
