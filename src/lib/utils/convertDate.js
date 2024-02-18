42a4936e5bd3b225e2680eb6869c0049fc9e1ec7
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-18 19:01:32 +0700

    feat: add new function for convert date range

--- src/lib/utils/convertDate.js
+++ src/lib/utils/convertDate.js
@@ -11,3 +11,24 @@ export const convertDate = (data) => {
   const result = `${parts[0]}${separator}${parts[1]}${separator}${parts[2]}`;
   return result;
 };
+
+export const converDateRange = (data) => {
+  const dateArr = data?.split(",");
+  const dateStart = new Date(dateArr[0]);
+  const dateEnd = new Date(dateArr[1]);
+  const formattedDateStart = dateStart?.toLocaleDateString("id-ID", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedDateEnd = dateEnd?.toLocaleDateString("id-ID", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
+
+  const result = `${formattedDateStart} - ${formattedDateEnd}`;
+
+  return result;
+};
