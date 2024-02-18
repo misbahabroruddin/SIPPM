d3bea51012b8fa9b23aa095828d1f2faa8c81ccb
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-18 23:35:02 +0700

    feat: add hooks for menu based on role

---
+++ src/lib/constants/sidebar.js
@@ -0,0 +1,175 @@
+export const MENU_SIDEBAR_ADMIN = [
+  {
+    label: "Dashboard",
+    icon: "/icons/dashboard.svg",
+    link: "/dashboard",
+  },
+  {
+    label: "Biodata",
+    icon: "/icons/biodata.svg",
+    link: "/biodata",
+  },
+  {
+    label: "Anggota",
+    icon: "/icons/anggota.svg",
+    link: "/anggota",
+  },
+  {
+    label: "Data Referensi",
+    icon: "/icons/data-referensi.svg",
+    link: "/data-referesi",
+    children: [
+      {
+        label: "Bidang Ilmu",
+        icon: "/icons/sub-menu.svg",
+        link: "/data-referesi/bidang-ilmu",
+      },
+      {
+        label: "Kabupaten",
+        icon: "/icons/sub-menu.svg",
+        link: "/data-referesi/kabupaten",
+      },
+      {
+        label: "Jabatan Fungsional",
+        icon: "/icons/sub-menu.svg",
+        link: "/data-referesi/jabatan-fungsional",
+      },
+      {
+        label: "Program Studi",
+        icon: "/icons/sub-menu.svg",
+        link: "/data-referesi/program-studi",
+      },
+      {
+        label: "Jenis Penelitian",
+        icon: "/icons/sub-menu.svg",
+        link: "/data-referesi/jenis-penelitian",
+      },
+      {
+        label: "Rumpun Ilmu",
+        icon: "/icons/sub-menu.svg",
+        link: "/data-referesi/rumpun-ilmu",
+      },
+      {
+        label: "Luaran Wajib",
+        icon: "/icons/sub-menu.svg",
+        link: "/data-referesi/luaran-wajib",
+      },
+    ],
+  },
+];
+
+export const MENU_SIDEBAR_DOSEN = [
+  {
+    label: "Dashboard",
+    icon: "/icons/dashboard.svg",
+    link: "/dashboard",
+  },
+  {
+    label: "Biodata",
+    icon: "/icons/biodata.svg",
+    link: "/biodata",
+  },
+  {
+    label: "Proposal",
+    icon: "/icons/proposal.svg",
+    link: "/proposal",
+  },
+  {
+    label: "Penilaian",
+    icon: "/icons/penilaian.svg",
+    link: "/penilaian",
+  },
+  {
+    label: "Monev",
+    icon: "/icons/monev.svg",
+    link: "/monev",
+  },
+  {
+    label: "Document",
+    icon: "/icons/document.svg",
+    link: "/document",
+  },
+  {
+    label: "Insentif",
+    icon: "/icons/insentif.svg",
+    link: "/insentif",
+  },
+  {
+    label: "Repository",
+    icon: "/icons/repository.svg",
+    link: "/repository",
+  },
+  {
+    label: "Laporan Hasil",
+    icon: "/icons/laporan-hasil.svg",
+    link: "/laporan-hasil",
+  },
+];
+
+export const MENU_SIDEBAR_LPPM = [
+  {
+    label: "Dashboard",
+    icon: "/icons/dashboard.svg",
+    link: "/dashboard",
+  },
+  {
+    label: "Dosen",
+    icon: "/icons/dosen.svg",
+    link: "/dosen",
+  },
+  {
+    label: "Proposal",
+    icon: "/icons/proposal.svg",
+    link: "/proposal",
+  },
+  {
+    label: "Document",
+    icon: "/icons/document.svg",
+    link: "/document",
+    children: [
+      {
+        label: "Penelitian",
+        icon: "/icons/sub-menu.svg",
+        link: "/document/penelitian",
+      },
+      {
+        label: "PKM",
+        icon: "/icons/sub-menu.svg",
+        link: "/document/pkm",
+      },
+    ],
+  },
+  {
+    label: "Laporan",
+    icon: "/icons/document.svg",
+    link: "/laporan",
+  },
+];
+
+export const MENU_SIDEBAR_REVIEWER = [
+  {
+    label: "Dashboard",
+    icon: "/icons/dashboard.svg",
+    link: "/dashboard",
+  },
+  {
+    label: "Dosen",
+    icon: "/icons/dosen.svg",
+    link: "/dosen",
+  },
+  {
+    label: "Proposal",
+    icon: "/icons/proposal.svg",
+    link: "/proposal",
+  },
+  {
+    label: "Document",
+    icon: "/icons/document.svg",
+    link: "/document",
+  },
+  {
+    label: "Laporan",
+    icon: "/icons/document.svg",
+    link: "/laporan",
+  },
+];
---
+++ src/lib/datas/stepArray.js
@@ -0,0 +1,40 @@
+const pathIcon = "/icons/stepper/penelitian/";
+
+export const steps = [
+  {
+    id: 1,
+    label: "Identitas Usulan",
+    iconActive: `${pathIcon}id-active.svg`,
+    iconComplete: `${pathIcon}id-complete.svg`,
+  },
+  {
+    id: 2,
+    label: "Anggota",
+    iconActive: `${pathIcon}friend-active.svg`,
+    iconComplete: `${pathIcon}friend-complete.svg`,
+  },
+  {
+    id: 3,
+    label: "Luaran Dan Target Capaian",
+    iconActive: `${pathIcon}pied-piper-active.svg`,
+    iconComplete: `${pathIcon}pied-piper-complete.svg`,
+  },
+  {
+    id: 4,
+    label: "Rencana Anggaran",
+    iconActive: `${pathIcon}invoice-active.svg`,
+    iconComplete: `${pathIcon}invoice-complete.svg`,
+  },
+  {
+    id: 5,
+    label: "Jadwal",
+    iconActive: `${pathIcon}calender-active.svg`,
+    iconComplete: `${pathIcon}calender-complete.svg`,
+  },
+  {
+    id: 6,
+    label: "Berkas",
+    iconActive: `${pathIcon}upload-active.svg`,
+    iconComplete: `${pathIcon}calender-complete.svg`,
+  },
+];
