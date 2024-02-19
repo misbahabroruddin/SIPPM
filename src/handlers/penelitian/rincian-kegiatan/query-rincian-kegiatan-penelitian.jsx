a5093a69348c1a919dae5b7ec66097dc0ab1aaf6
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-19 11:08:50 +0700

    feat: add handler for get all rincian kegiatan penelitian

---
+++ src/handlers/penelitian/rincian-kegiatan/query-rincian-kegiatan-penelitian.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+import { useParams } from "next/navigation";
+
+export const useQueryRincianKegiatanPenelitian = () => {
+  const axios = useAxios();
+  const { id } = useParams();
+
+  const fetchRincianKegiatan = async () => {
+    try {
+      const penelitianId = localStorage.getItem("penelitianId");
+      const { data } = await axios.get(
+        `/proposals/dosen/penelitians/${penelitianId || id}/rincian-kegiatans`
+      );
+      return data.data;
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const {
+    data: rincianKegiatanPenelitian,
+    isLoading: isLoadingRincianKegiatanPenelitian,
+    refetch: refetchRincianKegiatanPenelitian,
+  } = useQuery({
+    queryKey: ["rincianKegiatanPenelitian"],
+    queryFn: fetchRincianKegiatan,
+  });
+
+  return {
+    rincianKegiatanPenelitian,
+    isLoadingRincianKegiatanPenelitian,
+    refetchRincianKegiatanPenelitian,
+  };
+};
