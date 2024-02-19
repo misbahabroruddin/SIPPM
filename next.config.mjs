17861a11d68b990bbaba8f6e2310e8df1df2d276
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-19 11:16:42 +0700

    feat: configure next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "sippm.unsia.ac.id",
+      },
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+      },
+      {
+        protocol: "https",
+        hostname: "backend-dev.unsia.ac.id",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
