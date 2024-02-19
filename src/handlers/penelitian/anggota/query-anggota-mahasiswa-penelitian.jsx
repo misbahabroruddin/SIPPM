306ef8da85457a43f674764f2c7cf3b7a577f27b
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-19 11:04:26 +0700

    feat: add handler for get all anggota penelitian mahasiswa

---
+++ src/handlers/penelitian/anggota/query-anggota-mahasiswa-penelitian.jsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { toast } from "react-toastify";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+
+export const useQueryAnggotaMahasiswaPenelitian = () => {
+  const axios = useAxios();
+  const { id } = useParams();
+
+  const fetchAnggotaMahasiswaPenelitian = async () => {
+    try {
+      const penelitianId = localStorage.getItem("penelitianId");
+      const { data } = await axios.get(
+        `proposals/dosen/penelitians/${
+          penelitianId || id
+        }/anggotas?jenis_anggota=Mahasiswa`
+      );
+      return data.data;
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const {
+    data: listAnggotaMahasiswaPenelitian,
+    refetch: refetchAnggotaMahasiswaPenelitan,
+    isLoading: isLoadingAnggotaMahasiswaPenelitan,
+  } = useQuery({
+    queryKey: ["anggotaMahasiswa"],
+    queryFn: fetchAnggotaMahasiswaPenelitian,
+  });
+
+  return {
+    listAnggotaMahasiswaPenelitian,
+    refetchAnggotaMahasiswaPenelitan,
+    isLoadingAnggotaMahasiswaPenelitan,
+  };
+};
