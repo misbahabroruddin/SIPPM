65476aa6276f098410dbacb2155ca84f4c5d9b4b
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-19 11:11:36 +0700

    feat: add handler for get detail anggota dosen pkm

---
+++ src/handlers/pengabdian/anggota/query-detaill-anggota-dosen-pkm.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { useParams } from "next/navigation";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+
+export const useQueryDetailAnggotaDosenPKM = (anggotaId) => {
+  const axios = useAxios();
+  const { id } = useParams();
+  const pengabdianId = localStorage.getItem("pengabdianId");
+
+  const fetchDetailAnggotaDosen = async () => {
+    try {
+      const { data } = await axios.get(
+        `/proposals/dosen/pkms/${pengabdianId || id}/anggotas/${anggotaId}`
+      );
+      return data.data;
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const { data: detail, isLoading } = useQuery({
+    queryKey: ["detailAnggotaDosenPKM", anggotaId],
+    queryFn: fetchDetailAnggotaDosen,
+  });
+
+  return {
+    detail,
+    isLoading,
+  };
+};
