a4dbce267f4ccd5c61e8a9325fcfe1db12bc30fa
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-19 11:05:21 +0700

    feat: add handler for upload berkas penilitian

---
+++ src/handlers/penelitian/berkas/upload-berkas-penelitian.jsx
@@ -0,0 +1,60 @@
+"use client";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+import { useStep } from "@/lib/hooks/useStep";
+import { useParams } from "next/navigation";
+
+export const useUploadBerkasPenelitian = (router) => {
+  const axios = useAxios();
+  const { setCurrentStep } = useStep();
+  const { id } = useParams();
+  const queryClient = useQueryClient();
+
+  const onSubmit = async (form) => {
+    const formData = new FormData();
+    formData.append("file_cv", form.file_cv[0]);
+    formData.append("file_proposal", form.file_proposal[0]);
+    formData.append("file_pernyataan_mitra", form.file_pernyataan_mitra[0]);
+
+    try {
+      const penelitianId = localStorage.getItem("penelitianId");
+      const { data } = await axios.post(
+        `/proposals/dosen/penelitians/${penelitianId || id}/berkas`,
+        formData
+      );
+      localStorage.removeItem("penelitianId");
+      localStorage.removeItem("step");
+      localStorage.removeItem("isEdit");
+      router.push("/proposal");
+      setCurrentStep(1);
+      toast.success("Penelitian berhasil diajukan");
+      return data;
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const { mutateAsync: uploadBerkas, isPending: isLoadingSubmit } = useMutation(
+    {
+      mutationFn: onSubmit,
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ["listPenelitian"],
+        });
+        queryClient.resetQueries({
+          queryKey: ["anggotaMahasiswa"],
+        });
+        queryClient.resetQueries({
+          queryKey: ["anggotaDosen"],
+        });
+      },
+    }
+  );
+
+  return {
+    uploadBerkas,
+    isLoadingSubmit,
+  };
+};
