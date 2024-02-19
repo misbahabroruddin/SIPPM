cba6f1e2638b33499f4a596b8724c4ed3b55fec3
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-19 11:04:15 +0700

    feat: add handler for get all anggota penelitian dosen

---
+++ src/handlers/penelitian/anggota/query-anggota-dosen-penelitian.jsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { toast } from "react-toastify";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+
+export const useQueryAnggotaDosenPenelitian = () => {
+  const axios = useAxios();
+  const { id } = useParams();
+
+  const fetchAnggotaDosenPenelitian = async () => {
+    try {
+      const penelitianId = localStorage.getItem("penelitianId");
+      const { data } = await axios.get(
+        `proposals/dosen/penelitians/${
+          penelitianId || id
+        }/anggotas?jenis_anggota=Dosen`
+      );
+      return data.data;
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const {
+    data: listAnggotaDosenPenelitian,
+    refetch: refetchAnggotaDosenPenelitan,
+    isLoading: isLoadingAnggotaDosenPenelitan,
+  } = useQuery({
+    queryKey: ["anggotaDosen"],
+    queryFn: fetchAnggotaDosenPenelitian,
+  });
+
+  return {
+    listAnggotaDosenPenelitian,
+    refetchAnggotaDosenPenelitan,
+    isLoadingAnggotaDosenPenelitan,
+  };
+};
