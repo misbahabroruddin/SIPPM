598f7b2551de2c1caebfd4bc6ac7e81d0e17671e
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-20 22:50:01 +0700

    style: fix styling for error message

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -18,32 +18,42 @@ export const Input = ({
   ...props
 }) => {
   return (
-    <div className={twMerge("flex items-center", containerClass)}>
-      {label && (
-        <Label
-          className={labelClass}
-          htmlFor={name}
-          text={label}
-          required={required}
-        />
-      )}
-      <div className='w-full ml-[1px] '>
-        <input
-          type={type}
-          name={name}
-          id={name}
-          placeholder={placeholder}
-          {...register}
-          className={twMerge(
-            "w-full px-3 py-2 outline outline-1 outline-secondary-200 rounded-md focus:outline-none focus:ring-1 focus:ring-primary focus:border-transparent placeholder:text-sm",
-            errors && "outline-red-500",
-            inputClass
-          )}
-          autoComplete='off'
-          defaultValue={defaultValue}
-          {...props}
-        />
+    <div className='flex flex-col'>
+      <div className={twMerge("flex items-center", containerClass)}>
+        {label && (
+          <Label
+            className={labelClass}
+            htmlFor={name}
+            text={label}
+            required={required}
+          />
+        )}
+        <div className='w-full ml-[1px] '>
+          <input
+            type={type}
+            name={name}
+            id={name}
+            placeholder={placeholder}
+            {...register}
+            className={twMerge(
+              "w-full px-3 py-2 outline outline-1 outline-secondary-200 rounded-md focus:outline-none focus:ring-1 focus:ring-primary focus:border-transparent placeholder:text-sm disabled:bg-gray-200 disabled:cursor-not-allowed",
+              errors && "outline-red-500",
+              inputClass
+            )}
+            autoComplete='off'
+            defaultValue={defaultValue}
+            {...props}
+          />
+        </div>
       </div>
+      {errors && (
+        <div className='flex'>
+          <span className='w-1/2'></span>
+          <span className='w-full text-red-600 text-sm'>
+            * {errors?.message}
+          </span>
+        </div>
+      )}
     </div>
   );
 };
