a3f3225614e5eddae3793a9a6e829abb7105b8cd
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-20 09:35:39 +0700

    fix: fix styling button add

--- src/components/button/button-add.jsx
+++ src/components/button/button-add.jsx
@@ -1,11 +1,20 @@
 "use client";
 
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
-export const ButtonAdd = ({ onClick, text = "Pengajuan", ...props }) => {
+export const ButtonAdd = ({
+  onClick,
+  text = "Pengajuan",
+  className,
+  ...props
+}) => {
   return (
     <button
-      className='flex gap-2 items-center px-4 py-2 bg-[#23B900] rounded text-white disabled:bg-gray-500 disabled:cursor-not-allowed'
+      className={twMerge(
+        "flex gap-2 items-center px-4 py-2 bg-[#23B900] rounded text-white disabled:bg-gray-500 disabled:cursor-not-allowed",
+        className
+      )}
       onClick={onClick}
       {...props}
     >
@@ -15,7 +24,7 @@ export const ButtonAdd = ({ onClick, text = "Pengajuan", ...props }) => {
         height={24}
         alt='tab'
       />
-      <p>{text}</p>
+      <p className='hidden lg:block'>{text}</p>
     </button>
   );
 };
