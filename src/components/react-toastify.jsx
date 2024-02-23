1f9b65cb5fd6c613a636fc706ec987e983a81f92
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-23 17:42:32 +0700

    fix: modify react toastify

--- src/components/react-toastify.jsx
+++ src/components/react-toastify.jsx
@@ -8,13 +8,14 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Toast = () => (
   <ToastContainer
-    position='top-center'
+    position="top-center"
     autoClose={5000}
     closeOnClick
     draggable
     pauseOnHover
     pauseOnFocusLoss={false}
     hideProgressBar
+    theme="colored"
   />
 );
 
