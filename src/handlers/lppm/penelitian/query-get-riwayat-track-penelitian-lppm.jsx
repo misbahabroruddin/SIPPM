2102bd4705eb5cd47af46e2cff4ac03dfc4d124a
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-23 18:53:16 +0700

    feat: add handler for get riwayat track

---
+++ src/handlers/lppm/penelitian/query-get-riwayat-track-penelitian-lppm.jsx
@@ -0,0 +1,32 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { notFound, useParams } from "next/navigation";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+
+export const useQueryGetRiwayatTrackPenelitianLPPM = () => {
+  const axios = useAxios();
+  const { id } = useParams();
+
+  const { data, status, isLoading, refetch } = useQuery({
+    queryKey: ["riwayatPenelitianLPPM", id],
+    queryFn: async () => {
+      try {
+        const { data } = await axios.get(`/lppms/penelitians/${id}`);
+        return data.data;
+      } catch (error) {
+        toast.error("Error: " + error.message);
+      }
+    },
+  });
+
+  if (status === "error") return notFound();
+
+  return {
+    data,
+    isLoading,
+    status,
+    refetch,
+  };
+};
---
+++ src/handlers/lppm/pengabdian/query-get-riwayat-track.jsx
@@ -0,0 +1,32 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { notFound, useParams } from "next/navigation";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+
+export const useQueryGetRiwayatTrackPKM = () => {
+  const axios = useAxios();
+  const { id } = useParams();
+
+  const { data, status, isLoading, refetch } = useQuery({
+    queryKey: ["riwayatPengabdianLPPM", id],
+    queryFn: async () => {
+      try {
+        const { data } = await axios.get(`/lppms/pkms/${id}`);
+        return data.data;
+      } catch (error) {
+        toast.error(error.message);
+      }
+    },
+  });
+
+  if (status === "error") return notFound();
+
+  return {
+    data,
+    isLoading,
+    status,
+    refetch,
+  };
+};
