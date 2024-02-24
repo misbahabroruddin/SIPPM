29681f5aa6f1b565c1710f9ae5b85fe9bcf5d83f
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-24 07:44:03 +0700

    feat: create proposal page for lppm

---
+++ src/app/(sippm)/proposal/components/views/proposal-page-lppm.jsx
@@ -0,0 +1,50 @@
+"use client";
+import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+import { BasePageTitle } from "@/components/base-page-title";
+import { ContainerPage } from "@/components/container-page";
+import { Tabs } from "../tabs";
+import { SearchInput } from "@/components/input/search-input";
+import { useDebouncedCallback } from "use-debounce";
+import { CardDashboard } from "@/components/card/card-dashboard";
+
+export default function ProposalPageLPPM() {
+  const [tabActive] = useState("penelitian");
+  const [searchPenelitian, setSearchPenelitian] = useState("");
+  const [searchPengabdian, setSearchPengabdian] = useState("");
+  const tabParams = useSearchParams();
+  const currentTab = tabParams.get("tab");
+  const debounced = useDebouncedCallback((value) => {
+    setSearchPenelitian(value);
+  }, 1000);
+  return (
+    <ContainerPage>
+      <div className="flex flex-col gap-4">
+        <BasePageTitle iconSrc="/icons/search-black.svg" title="Proposal" />
+        <div className="flex justify-between">
+          <div className="flex items-center gap-2 lg:gap-4">
+            <Tabs tabActive={currentTab || tabActive} />
+            <SearchInput
+              onChange={(e) => {
+                currentTab === "pengabdian"
+                  ? setSearchPengabdian(e.target.value)
+                  : debounced(e.target.value);
+              }}
+              defaultValue={
+                currentTab === "pengabdian"
+                  ? searchPengabdian
+                  : searchPenelitian
+              }
+            />
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <CardDashboard />
+          <CardDashboard status="Revisi" />
+          <CardDashboard status="Ditolak" />
+        </div>
+      </div>
+    </ContainerPage>
+  );
+}
