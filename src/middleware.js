8a5ef36f653a1d1eaa27969213d54237f97b515a
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-24 07:56:06 +0700

    fix: config middleware nextjs

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,24 @@
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
+
+import { DOSEN } from "./lib/constants/role";
+import { DOSEN_PATHS } from "./lib/datas/dosen-path";
 
 const middleware = async (request) => {
   try {
     const path = request.nextUrl.pathname;
     const nextAuthToken = request.cookies.get("next-auth.session-token");
+
+    const token = await getToken({
+      req: request,
+      secret: process.env.AUTH_SECRET,
+    });
+
+    const role = token?.roles[0].name;
+
+    if (role !== DOSEN && DOSEN_PATHS.includes(path)) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
 
     if (!nextAuthToken && path !== "/login") {
       return NextResponse.redirect(new URL("/login", request.url));
@@ -26,6 +41,8 @@ export const config = {
     "/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)",
     "/",
     "/login",
+    "/proposal/penelitian/edit/[id]",
+    "/proposal/penelitian/track/[id]",
   ],
 };
 
