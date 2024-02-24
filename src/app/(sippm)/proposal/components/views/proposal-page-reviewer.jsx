93a273d7af9818f71d2ed09052956b743f48053f
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-24 07:46:04 +0700

    feat: create proposal page reviewer

---
+++ src/app/(sippm)/proposal/components/views/proposal-page-reviewer.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { ContainerPage } from "@/components/container-page";
+
+export default function ProposalPageReviewer() {
+  return <ContainerPage>ProposalPageReviewer</ContainerPage>;
+}
