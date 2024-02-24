de32d6b76a1d87fbf743f98ea65a096248915a04
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-24 07:41:55 +0700

    feat: add profile component for biodata page

---
+++ src/app/(sippm)/biodata/components/profile.jsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
+import { useGetProfile } from "@/handlers/biodata/query-get-profile";
+
+export const Profile = () => {
+  const { data: profile } = useGetProfile();
+  const { data: session } = useSession();
+
+  return (
+    <div className="flex h-fit basis-[318.87px] flex-col rounded-lg shadow-custom">
+      <div className="flex flex-col items-center justify-center gap-2 border-b-[0.5px] border-[#CCCCCC] p-4">
+        <Image
+          className="inline-block rounded-full"
+          src={session?.user.avatar}
+          width={280}
+          height={280}
+          alt="Profile"
+        />
+        <p className="mb-1 text-center text-2xl text-[#666666]">
+          {profile?.name}
+        </p>
+      </div>
+      <div className="flex flex-col gap-2 px-[18px] py-5">
+        <h2 className="text-sm font-[500]">Details</h2>
+        <div className="flex gap-2">
+          <div className="flex flex-col gap-2 text-sm font-[500]">
+            <p>Role</p>
+            <p>Username</p>
+            <p>NIP</p>
+            <p>Kontak</p>
+            <p>Email</p>
+          </div>
+          <div className="flex flex-col gap-2 text-sm font-[500] text-[#666666]">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <p key={index}>:</p>
+            ))}
+          </div>
+          <div className="flex flex-col gap-2 text-sm font-[500] text-[#666666]">
+            <p>{session?.user.roles[0].name}</p>
+            <p>{profile?.username}</p>
+            <p>{profile?.nip || "-"}</p>
+            <p>{profile?.kontak || "-"}</p>
+            <p>{profile?.email}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
