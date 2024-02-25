266d10859ab0573d2a4f7d827408a8be6838c989
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-25 11:54:02 +0700

    feat: create track penelitian dosen page

---
+++ src/app/(sippm)/proposal/penelitian/track/[id]/components/views/track-penelitian-dosen.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+import { BasePageTitle } from "@/components/base-page-title";
+import { capitalFirtsLatter } from "@/lib/utils/capitalizeFirstLetter";
+import { Timeline } from "@/components/timeline";
+import { RiwayatPenelitianLPPM } from "@/components/riwayat/penelitian/lppm/riwayat-penelitian-lppm";
+import { RiwayatPenelitianReviewer } from "@/components/riwayat/penelitian/reviewer/riwayat-penelitian-reviewer";
+
+export default function TrackPenelitianDosenPage() {
+  const path = usePathname();
+  const pathArr = path.split("/");
+  return (
+    <div className="flex flex-col gap-4">
+      <BasePageTitle
+        iconSrc="/icons/search-black.svg"
+        title={capitalFirtsLatter(pathArr[2])}
+      />
+      <Timeline>
+        <RiwayatPenelitianLPPM />
+        <RiwayatPenelitianReviewer />
+      </Timeline>
+    </div>
+  );
+}
