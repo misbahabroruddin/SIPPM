a8160e66ac49f85f8694fac93e8cea24e83b150b
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-25 11:05:30 +0700

    feat: query get listing penelitian for lppm

---
+++ src/handlers/lppm/penelitian/query-get-listing-penelitian.jsx
@@ -0,0 +1,31 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+
+export const useQueryGetPenelitianLPPM = (search) => {
+  const axios = useAxios();
+
+  const query = useQuery({
+    queryKey: ["listingPenelitianLPPM"],
+    queryFn: async () => {
+      let params;
+      if (search) {
+        params = {
+          judul: search,
+        };
+      }
+      try {
+        const { data } = await axios.get("/proposals/lppms/penelitians", {
+          params: params,
+        });
+        return data?.data;
+      } catch (error) {
+        toast.error(error.message);
+      }
+    },
+  });
+
+  return { ...query };
+};
