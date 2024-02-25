838326843fdadcaa3442848911f8a8faa7cbe476
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-25 11:53:41 +0700

    feat: create query for get listing pkm lppm

---
+++ src/handlers/lppm/pengabdian/query-get-listing-pkm-lppm.jsx
@@ -0,0 +1,31 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+
+export const useQueryGetPengabdianLPPM = (search) => {
+  const axios = useAxios();
+
+  const query = useQuery({
+    queryKey: ["listingPengabdianLPPM"],
+    queryFn: async () => {
+      let params;
+      if (search) {
+        params = {
+          judul: search,
+        };
+      }
+      try {
+        const { data } = await axios.get("/proposals/lppms/pkms", {
+          params: params,
+        });
+        return data?.data;
+      } catch (error) {
+        toast.error(error.message);
+      }
+    },
+  });
+
+  return { ...query };
+};
