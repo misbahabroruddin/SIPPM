956cbcc06e9fe614c49ad1b3da976ca453e4a8cc
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-26 14:02:02 +0700

    feat: create table rincian kegiatan for detail penelitian at lppm

---
+++ src/app/(sippm)/proposal/penelitian/track/[id]/components/table-detail-rincian-kegiatan.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { converDateRange } from "@/lib/utils/convertDate";
+import { Typography } from "@material-tailwind/react";
+
+export const TableDetailRincianKegiatan = ({ data }) => {
+  const header = ["No", "Kegiatan", "Waktu"];
+
+  return (
+    <table className="w-full min-w-max table-auto overflow-hidden rounded-t-lg text-left">
+      <thead className="rounded-lg">
+        <tr>
+          {header.map((head) => (
+            <th key={head} className="  bg-primary p-4 last:text-center">
+              <Typography
+                variant="small"
+                color="blue-gray"
+                className="font-poppins font-semibold leading-none text-white"
+              >
+                {head}
+              </Typography>
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data?.data?.rincian_kegiatan_proposals.length ? (
+          data?.data?.rincian_kegiatan_proposals.map((row, index) => (
+            <tr key={row.id} className="text-base even:bg-sky">
+              <td className="w-8 p-3">
+                <Typography
+                  color="blue-gray"
+                  className="text-center font-normal"
+                >
+                  {index + 1}
+                </Typography>
+              </td>
+              <td className="p-3 ">
+                <Typography color="blue-gray" className="font-normal">
+                  {row.kegiatan}
+                </Typography>
+              </td>
+              <td className="p-3 text-center">
+                <Typography color="blue-gray" className="font-normal">
+                  {converDateRange(row.waktu)}
+                </Typography>
+              </td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td className="p-4 text-center" colSpan={4}>
+              Tidak ada data
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+};
