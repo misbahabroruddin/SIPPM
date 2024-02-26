4e53a8324d60e1be20cf37c10356ff213e3fa5d9
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-26 11:32:56 +0700

    feat: integrate with API

--- src/app/(sippm)/proposal/penelitian/track/[id]/components/section-header-detail-proposal.jsx
+++ src/app/(sippm)/proposal/penelitian/track/[id]/components/section-header-detail-proposal.jsx
@@ -1,9 +1,9 @@
 "use client";
 
-export const SectionHeaderDetailProposal = () => {
+export const SectionHeaderDetailProposal = ({ data }) => {
   return (
     <div className="flex flex-col gap-2">
-      <h1 className="text-xl font-[500]">Judul Penelitian</h1>
+      <h1 className="text-xl font-[500]">{data?.judul}</h1>
       <div className="flex w-3/4 justify-between text-black-07">
         <div className="flex w-2/5 justify-between">
           <div className="flex grow justify-between">
@@ -18,27 +18,27 @@ export const SectionHeaderDetailProposal = () => {
             <div className="flex flex-col gap-2">
               <div className="flex gap-4">
                 <span>:</span>
-                <p>juduadwasdwadwl</p>
+                <p>{data?.user?.biodata?.nama_lengkap}</p>
               </div>
               <div className="flex gap-4">
                 <span>:</span>
-                <p>juduadwasdwadwladawdasdw</p>
+                <p>{`${data?.user?.biodata?.tempat_lahir || ""} ${data?.user?.biodata?.tanggal_lahir}`}</p>
               </div>
               <div className="flex gap-4">
                 <span>:</span>
-                <p>juduadwasdwadwladawdasdw</p>
+                <p>{data?.user?.biodata.nidn_or_nidk_or_nim}</p>
               </div>
               <div className="flex gap-4">
                 <span>:</span>
-                <p>juduadwasdwadwladawdasdw</p>
+                <p>{data?.user?.biodata.pangkat_golongan || "-"}</p>
               </div>
               <div className="flex gap-4">
                 <span>:</span>
-                <p>juduadwasdwadwladawdasdw</p>
+                <p>{data?.user?.biodata.jabatan_fungsional?.nama || "-"}</p>
               </div>
               <div className="flex gap-4">
                 <span>:</span>
-                <p>juduadwasdwadwladawdasdw</p>
+                <p>{data?.user?.biodata.program_studi?.nama || "-"}</p>
               </div>
             </div>
           </div>
@@ -55,23 +55,23 @@ export const SectionHeaderDetailProposal = () => {
             <div className="flex flex-col gap-2">
               <div className="flex gap-4">
                 <span>:</span>
-                <p>juduadwasdwadwl</p>
+                <p>{data?.user?.biodata?.email}</p>
               </div>
               <div className="flex gap-4">
                 <span>:</span>
-                <p>juduadwasdwadwladawdasdw</p>
+                <p>{data?.user?.biodata?.nomor_hp}</p>
               </div>
               <div className="flex gap-4">
                 <span>:</span>
-                <p>juduadwasdwadwladawdasdw</p>
+                <p>{data?.user?.biodata?.sinta_id || "-"}</p>
               </div>
               <div className="flex gap-4">
                 <span>:</span>
-                <p>juduadwasdwadwladawdasdw</p>
+                <p>{data?.user?.biodata?.scopus_id || "-"}</p>
               </div>
               <div className="flex gap-4">
                 <span>:</span>
-                <p>juduadwasdwadwladawdasdw</p>
+                <p>{data?.user?.biodata?.google_scholar_id || "-"}</p>
               </div>
             </div>
           </div>
