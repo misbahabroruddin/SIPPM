011f4bafd2fc25867e8f4dacc8f0d77599c5dadd
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-26 10:46:18 +0700

    feat: create handler for get detail penelitian lppm

--- src/handlers/lppm/penelitian/query-get-detail-penelitian.jsx
+++ src/handlers/lppm/penelitian/query-get-detail-penelitian.jsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useAxios } from "@/lib/hooks/useAxios";
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
+import { toast } from "react-toastify";
 
 export const useQueryDetailPenelitian = () => {
   const axios = useAxios();
@@ -9,7 +11,18 @@ export const useQueryDetailPenelitian = () => {
 
   const fetchDetailPenelitian = async () => {
     try {
-      const { data } = await axios.get(`/lppms/penelitians/${id}`);
-    } catch (error) {}
+      const { data } = await axios.get(`/proposals/lppms/penelitians/${id}`);
+
+      return data;
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
+
+  const query = useQuery({
+    queryKey: ["detailPenelitianLPPM", id],
+    queryFn: fetchDetailPenelitian,
+  });
+
+  return { ...query };
 };
