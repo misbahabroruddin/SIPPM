b679a67c5c4b2e060b97fc61c6042195fb3f93ef
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-26 17:20:34 +0700

    fix: fix style and add new props

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge";
 import { Label } from "../label";
 
 export const Input = ({
-  label = "Jenis Penelitian",
+  label,
   name,
   type,
   register,
@@ -15,10 +15,11 @@ export const Input = ({
   errors,
   required,
   defaultValue,
+  spanEmptyClass,
   ...props
 }) => {
   return (
-    <div className='flex flex-col'>
+    <div className="flex flex-col">
       <div className={twMerge("flex items-center", containerClass)}>
         {label && (
           <Label
@@ -28,7 +29,7 @@ export const Input = ({
             required={required}
           />
         )}
-        <div className='w-full ml-[1px] '>
+        <div className="ml-[1px] w-full ">
           <input
             type={type}
             name={name}
@@ -36,20 +37,20 @@ export const Input = ({
             placeholder={placeholder}
             {...register}
             className={twMerge(
-              "w-full px-3 py-2 outline outline-1 outline-secondary-200 rounded-md focus:outline-none focus:ring-1 focus:ring-primary focus:border-transparent placeholder:text-sm disabled:bg-gray-200 disabled:cursor-not-allowed",
+              "w-full rounded-md px-3 py-2 outline outline-1 outline-secondary-200 placeholder:text-sm focus:border-transparent focus:outline-none focus:ring-1 focus:ring-primary disabled:cursor-not-allowed disabled:bg-gray-200",
               errors && "outline-red-500",
-              inputClass
+              inputClass,
             )}
-            autoComplete='off'
+            autoComplete="off"
             defaultValue={defaultValue}
             {...props}
           />
         </div>
       </div>
       {errors && (
-        <div className='flex'>
-          <span className='w-1/2'></span>
-          <span className='w-full text-red-600 text-sm'>
+        <div className="flex">
+          <span className={twMerge("w-1/2", spanEmptyClass)}></span>
+          <span className="w-full text-sm text-red-600">
             * {errors?.message}
           </span>
         </div>
