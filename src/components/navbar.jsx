4b7ca46b1499d688a276a8429c64596b7f78da98
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-26 19:42:11 +0700

    fix: fixing error when signout

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,10 +8,10 @@ import { twMerge } from "tailwind-merge";
 
 const Path = (props) => (
   <path
-    fill='transparent'
-    strokeWidth='3'
-    stroke='black'
-    strokeLinecap='round'
+    fill="transparent"
+    strokeWidth="3"
+    stroke="black"
+    strokeLinecap="round"
     {...props}
   />
 );
@@ -22,19 +22,19 @@ const HamburgerMenu = ({ onClick, ...props }) => {
       className={twMerge("block cursor-pointer pr-2", props.className)}
       onClick={onClick}
     >
-      <svg width='18' height='18' viewBox='0 0 23 18'>
+      <svg width="18" height="18" viewBox="0 0 23 18">
         <Path
-          d='M 2 2.5 L 20 2.5'
-          className='top'
+          d="M 2 2.5 L 20 2.5"
+          className="top"
           variants={{
             closed: { d: "M 2 2.5 L 20 2.5" },
             open: { d: "M 3 16.5 L 17 2.5" },
           }}
         />
-        <Path d='M 2 9.423 L 20 9.423' opacity='1' className='middle' />
+        <Path d="M 2 9.423 L 20 9.423" opacity="1" className="middle" />
         <Path
-          d='M 2 16.346 L 20 16.346'
-          className='bottom'
+          d="M 2 16.346 L 20 16.346"
+          className="bottom"
           variants={{
             closed: { d: "M 2 16.346 L 20 16.346" },
             open: { d: "M 3 2.5 L 17 16.346" },
@@ -52,13 +52,13 @@ const Chevron = ({ onClick, ...props }) => {
       onClick={onClick}
     >
       <svg
-        height='18'
-        id='chevron-left'
-        viewBox='0 0 32 32'
-        width='18'
-        xmlns='http://www.w3.org/2000/svg'
+        height="18"
+        id="chevron-left"
+        viewBox="0 0 32 32"
+        width="18"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <path d='M20 1 L24 5 L14 16 L24 27 L20 31 L6 16 z' />
+        <path d="M20 1 L24 5 L14 16 L24 27 L20 31 L6 16 z" />
       </svg>
     </i>
   );
@@ -67,101 +67,101 @@ const Chevron = ({ onClick, ...props }) => {
 export const Navbar = () => {
   const { isOpen, toggleSidebar } = useSidebar();
   return (
-    <nav className='shadow  fixed top-0 r-0 bg-white w-full z-50'>
-      <div className='flex justify-between items-center py-4 px-7'>
+    <nav className="r-0  fixed top-0 z-[999] w-full bg-white shadow">
+      <div className="flex items-center justify-between px-7 py-4">
         <div
           className={twMerge(
-            "flex items-center gap-2 lg:gap-3 px-2 w-[236px] transition-all duration-200 justify-start",
-            isOpen ? "lg:justify-between" : ""
+            "flex w-[236px] items-center justify-start gap-2 px-2 transition-all duration-200 lg:gap-3",
+            isOpen ? "lg:justify-between" : "",
           )}
         >
           {isOpen ? (
             <>
-              <Link href='/' className='transition-all duration-200'>
-                <div className='flex gap-3 items-center'>
+              <Link href="/" className="transition-all duration-200">
+                <div className="flex items-center gap-3">
                   <Image
-                    src='/unsia.png'
-                    className='hidden lg:block w-auto'
+                    src="/unsia.png"
+                    className="hidden w-auto lg:block"
                     width={92}
                     height={100}
-                    alt='UNSIA'
+                    alt="UNSIA"
                   />
                   <Image
-                    src='/logo-unsia.svg'
-                    className='block lg:hidden w-auto'
+                    src="/logo-unsia.svg"
+                    className="block w-auto lg:hidden"
                     width={25}
                     height={25}
-                    alt='UNSIA'
+                    alt="UNSIA"
                   />
                 </div>
               </Link>
-              <Chevron onClick={toggleSidebar} className='hidden lg:block ' />
+              <Chevron onClick={toggleSidebar} className="hidden lg:block " />
               <HamburgerMenu
                 onClick={toggleSidebar}
-                className='block lg:hidden'
+                className="block lg:hidden"
               />
             </>
           ) : (
             <>
               <Link
-                href='/'
-                className='transition-all duration-200 hidden lg:block'
+                href="/"
+                className="hidden transition-all duration-200 lg:block"
               >
                 <Image
-                  className='w-auto'
-                  src='/logo-unsia.svg'
+                  className="w-auto"
+                  src="/logo-unsia.svg"
                   width={25}
                   height={25}
-                  alt='UNSIA'
+                  alt="UNSIA"
                 />
               </Link>
               <HamburgerMenu onClick={toggleSidebar} />
             </>
           )}
         </div>
-        <div className='block mr-[7px]'>
-          <div className='flex items-center justify-between p-2'>
+        <div className="mr-[7px] block">
+          <div className="flex items-center justify-between p-2">
             <button
-              onClick={() => signOut({ callbackUrl: "/" })}
-              className='px-4 py-1 rounded border border-primary text-black hover:bg-primary hover:text-white'
+              onClick={() => signOut()}
+              className="rounded border border-primary px-4 py-1 text-black hover:bg-primary hover:text-white"
             >
               Sign out
             </button>
-            <div className='px-1 py-[2px] cursor-pointer'>
+            <div className="cursor-pointer px-1 py-[2px]">
               <Image
-                className='w-auto hidden'
-                src='/icons/notif.svg'
+                className="hidden w-auto"
+                src="/icons/notif.svg"
                 width={19}
                 height={16}
-                alt='notif'
+                alt="notif"
               />
             </div>
-            <div className='flex gap-1 px-1 py-[2px] mx-4 cursor-pointer '>
+            <div className="mx-4 flex cursor-pointer gap-1 px-1 py-[2px] ">
               <Image
-                className='w-auto hidden'
-                src='/icons/globe.svg'
+                className="hidden w-auto"
+                src="/icons/globe.svg"
                 width={19}
                 height={16}
-                alt='notif'
+                alt="notif"
               />
-              <div className=' items-center hidden'>
-                <p className='font-[500] text-xl text-[#666666]'>EN</p>
+              <div className=" hidden items-center">
+                <p className="text-xl font-[500] text-[#666666]">EN</p>
               </div>
               <Image
-                src='/icons/arrow.svg'
+                src="/icons/arrow.svg"
                 width={12}
                 height={7}
-                alt='notif'
-                className='hidden'
+                alt="notif"
+                className="hidden"
               />
             </div>
-            <div className='px-1 py-[2px] cursor-pointer'>
+            <div className="cursor-pointer px-1 py-[2px]">
               <Image
-                className='w-auto hidden'
-                src='/icons/setting.svg'
+                className="hidden w-auto"
+                src="/icons/setting.svg"
                 width={19}
                 height={16}
-                alt='notif'
+                alt="notif"
               />
             </div>
           </div>
