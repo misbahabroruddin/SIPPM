adea43f6f5e389e0afc91c0b1f069aed37955c41
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-27 13:43:11 +0700

    fix: hide menu not availabel

--- src/lib/constants/sidebar.js
+++ src/lib/constants/sidebar.js
@@ -59,46 +59,46 @@ export const MENU_SIDEBAR_DOSEN = [
     icon: "/icons/dashboard.svg",
     link: "/dashboard",
   },
-  {
-    label: "Biodata",
-    icon: "/icons/biodata.svg",
-    link: "/biodata",
-  },
+  // {
+  //   label: "Biodata",
+  //   icon: "/icons/biodata.svg",
+  //   link: "/biodata",
+  // },
   {
     label: "Proposal",
     icon: "/icons/proposal.svg",
     link: "/proposal",
   },
-  {
-    label: "Penilaian",
-    icon: "/icons/penilaian.svg",
-    link: "/penilaian",
-  },
-  {
-    label: "Monev",
-    icon: "/icons/monev.svg",
-    link: "/monev",
-  },
-  {
-    label: "Document",
-    icon: "/icons/document.svg",
-    link: "/document",
-  },
-  {
-    label: "Insentif",
-    icon: "/icons/insentif.svg",
-    link: "/insentif",
-  },
-  {
-    label: "Repository",
-    icon: "/icons/repository.svg",
-    link: "/repository",
-  },
-  {
-    label: "Laporan Hasil",
-    icon: "/icons/laporan-hasil.svg",
-    link: "/laporan-hasil",
-  },
+  // {
+  //   label: "Penilaian",
+  //   icon: "/icons/penilaian.svg",
+  //   link: "/penilaian",
+  // },
+  // {
+  //   label: "Monev",
+  //   icon: "/icons/monev.svg",
+  //   link: "/monev",
+  // },
+  // {
+  //   label: "Document",
+  //   icon: "/icons/document.svg",
+  //   link: "/document",
+  // },
+  // {
+  //   label: "Insentif",
+  //   icon: "/icons/insentif.svg",
+  //   link: "/insentif",
+  // },
+  // {
+  //   label: "Repository",
+  //   icon: "/icons/repository.svg",
+  //   link: "/repository",
+  // },
+  // {
+  //   label: "Laporan Hasil",
+  //   icon: "/icons/laporan-hasil.svg",
+  //   link: "/laporan-hasil",
+  // },
 ];
 
 export const MENU_SIDEBAR_LPPM = [
@@ -107,38 +107,38 @@ export const MENU_SIDEBAR_LPPM = [
     icon: "/icons/dashboard.svg",
     link: "/dashboard",
   },
-  {
-    label: "Dosen",
-    icon: "/icons/dosen.svg",
-    link: "/dosen",
-  },
+  // {
+  //   label: "Dosen",
+  //   icon: "/icons/dosen.svg",
+  //   link: "/dosen",
+  // },
   {
     label: "Proposal",
     icon: "/icons/proposal.svg",
     link: "/proposal",
   },
-  {
-    label: "Document",
-    icon: "/icons/document.svg",
-    link: "/document",
-    children: [
-      {
-        label: "Penelitian",
-        icon: "/icons/sub-menu.svg",
-        link: "/document/penelitian",
-      },
-      {
-        label: "PKM",
-        icon: "/icons/sub-menu.svg",
-        link: "/document/pkm",
-      },
-    ],
-  },
-  {
-    label: "Laporan",
-    icon: "/icons/document.svg",
-    link: "/laporan",
-  },
+  // {
+  //   label: "Document",
+  //   icon: "/icons/document.svg",
+  //   link: "/document",
+  //   children: [
+  //     {
+  //       label: "Penelitian",
+  //       icon: "/icons/sub-menu.svg",
+  //       link: "/document/penelitian",
+  //     },
+  //     {
+  //       label: "PKM",
+  //       icon: "/icons/sub-menu.svg",
+  //       link: "/document/pkm",
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: "Laporan",
+  //   icon: "/icons/document.svg",
+  //   link: "/laporan",
+  // },
 ];
 
 export const MENU_SIDEBAR_REVIEWER = [
@@ -147,24 +147,24 @@ export const MENU_SIDEBAR_REVIEWER = [
     icon: "/icons/dashboard.svg",
     link: "/dashboard",
   },
-  {
-    label: "Dosen",
-    icon: "/icons/dosen.svg",
-    link: "/dosen",
-  },
+  // {
+  //   label: "Dosen",
+  //   icon: "/icons/dosen.svg",
+  //   link: "/dosen",
+  // },
   {
     label: "Proposal",
     icon: "/icons/proposal.svg",
     link: "/proposal",
   },
-  {
-    label: "Document",
-    icon: "/icons/document.svg",
-    link: "/document",
-  },
-  {
-    label: "Laporan",
-    icon: "/icons/document.svg",
-    link: "/laporan",
-  },
+  // {
+  //   label: "Document",
+  //   icon: "/icons/document.svg",
+  //   link: "/document",
+  // },
+  // {
+  //   label: "Laporan",
+  //   icon: "/icons/document.svg",
+  //   link: "/laporan",
+  // },
 ];
