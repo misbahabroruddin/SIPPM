9ef490605672175e44d9975a5692520353cd865b
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-27 12:02:32 +0700

    feat: create component tabs for detail pengabdian lppm

---
+++ src/app/(sippm)/proposal/pengabdian/track/[id]/components/tabs.jsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+
+import { Tab } from "@/components/tab";
+
+export const Tabs = ({ tabActive }) => {
+  const pathname = usePathname();
+  const tabParams = useSearchParams();
+  const router = useRouter();
+
+  const tabs = [
+    {
+      id: 1,
+      name: "dokumen",
+      icon: {
+        white: "/icons/location-white.svg",
+        black: "/icons/location-black.svg",
+      },
+      func: () => handleTabClick("dokumen"),
+    },
+    {
+      id: 2,
+      name: "riwayat",
+      icon: {
+        white: "/icons/history-white.svg",
+        black: "/icons/history-black.svg",
+      },
+      func: () => handleTabClick("riwayat"),
+    },
+  ];
+
+  const createQueryTab = useCallback(
+    (name, value) => {
+      const params = new URLSearchParams(tabParams.toString());
+      params.set(name, value);
+      return params.toString();
+    },
+    [tabParams],
+  );
+
+  const handleTabClick = (tabName) => {
+    router.push(`${pathname}?${createQueryTab("tab", tabName)}`);
+  };
+
+  return (
+    <div className="flex gap-2">
+      {tabs.map(({ id, name, icon, func }) => {
+        return (
+          <Tab
+            key={id}
+            tabName={name}
+            tabActive={tabActive}
+            iconSrc={icon}
+            onClick={func}
+          />
+        );
+      })}
+    </div>
+  );
+};
