aeb5df7a5d1a9ac1610a4b5daedb325e9d535adc
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-27 12:04:14 +0700

    feat: add handler for update verifikasi proposal

---
+++ src/handlers/lppm/pengabdian/update-proposal-pengabdian-lppm.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { toast } from "react-toastify";
+
+export const useUpdateProposalPengabdian = (reset, router) => {
+  const axios = useAxios();
+  const queryClient = useQueryClient();
+  const { id } = useParams();
+
+  const onSubmit = async (form) => {
+    try {
+      const { data } = await axios.put(
+        `/proposals/lppms/pkms/${id}`,
+        {
+          dana_yang_disetujui: form.dana_yang_disetujui,
+          status: form.status,
+          catatan: form.catatan,
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        },
+      );
+      reset();
+      router.push("/proposal");
+      return data;
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const mutate = useMutation({
+    mutationFn: onSubmit,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["listPengabdian"] });
+      queryClient.invalidateQueries({ queryKey: ["listingPengabdianLPPM"] });
+    },
+  });
+
+  return { ...mutate };
+};
