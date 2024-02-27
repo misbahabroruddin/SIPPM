34636b2206ea49f83b5addecbe71e6c2ee3de28b
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-27 11:57:15 +0700

    feat: create detail berkas component for detail pengabdian lppm

---
+++ src/app/(sippm)/proposal/pengabdian/track/[id]/components/detail-berkas.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { DetailListBerkas } from "./detail-list-berkas";
+import { FormVerifikasiUsulan } from "./form-verifikasi-usulan";
+
+export const DetailBerkas = ({ data }) => {
+  return (
+    <div className="flex flex-col gap-3">
+      <h4 className="text-lg font-[500]">Berkas</h4>
+      <DetailListBerkas data={data} />
+      {data?.data?.status_lppm !== "Diterima" && <FormVerifikasiUsulan />}
+    </div>
+  );
+};
