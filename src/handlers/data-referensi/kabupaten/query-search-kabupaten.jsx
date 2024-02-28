9308ed598110a4fe404e57cfa83462dbd7605a9a
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-28 10:53:12 +0700

    feat: create query get search kabupaten

---
+++ src/handlers/data-referensi/kabupaten/query-search-kabupaten.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+
+export const useQuerySearchKabupaten = () => {
+  const axios = useAxios();
+  const fetchSearchKabupaten = async () => {
+    try {
+      const { data } = await axios.get("/data-referensis/kabupatens/search");
+      const result = data.data.map((opt) => ({
+        value: opt.id,
+        label: opt.nama,
+      }));
+      return result;
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["kabupatenOptions"],
+    queryFn: fetchSearchKabupaten,
+  });
+
+  return {
+    data,
+    isLoading,
+  };
+};
