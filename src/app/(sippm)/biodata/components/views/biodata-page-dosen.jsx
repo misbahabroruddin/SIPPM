8dfcbd8b7d205de5362e0155cbbeea9e945e8ba7
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-28 14:40:08 +0700

    fix: disable ssr for form biodata

--- src/app/(sippm)/biodata/components/views/biodata-page-dosen.jsx
+++ src/app/(sippm)/biodata/components/views/biodata-page-dosen.jsx
@@ -1,11 +1,14 @@
 "use client";
 import { useState } from "react";
+import dynamic from "next/dynamic";
 import { useSearchParams } from "next/navigation";
 
 import { BasePageTitle } from "@/components/base-page-title";
 import { ContainerPage } from "@/components/container-page";
 import { Profile } from "../profile";
 import { Tabs } from "../tabs";
+
+const FormBiodata = dynamic(() => import("../form-biodata"), { ssr: false });
 
 export default function BiodataPageDosen() {
   const [tabActive] = useState("profile");
@@ -14,12 +17,21 @@ export default function BiodataPageDosen() {
 
   return (
     <ContainerPage>
-      <div className='flex flex-col gap-4'>
-        <BasePageTitle iconSrc='/icons/biodata-black.svg' title='Biodata' />
-        <section className='flex gap-4 ml-[7px]'>
+      <div className="flex flex-col gap-4">
+        <BasePageTitle iconSrc="/icons/biodata-black.svg" title="Biodata" />
+        <section className="ml-[7px] flex gap-4">
           <Profile />
-          <div className='flex flex-col gap-[15px] grow'>
+          <div className="flex grow flex-col gap-[15px]">
             <Tabs tabActive={currentTab || tabActive} />
+            <div className="flex flex-col gap-3 rounded-2xl border border-gray-200 shadow-custom">
+              <div className="relative flex flex-col gap-2 border-b border-gray-200 px-8 pt-4">
+                <h2 className="text-lg font-[500] text-primary">Biodata</h2>
+                {/* <p className="z-20 w-fit rounded-lg rounded-bl-none rounded-br-none border border-b-0 border-gray-200 px-2 py-1">
+                  Biodata
+                </p> */}
+              </div>
+              <FormBiodata />
+            </div>
           </div>
         </section>
       </div>
