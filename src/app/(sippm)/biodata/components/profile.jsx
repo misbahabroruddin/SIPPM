b0db2740952786ffdaca8a673d6de8e8bc87b41e
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-28 14:39:46 +0700

    fix: make conditional rendering when profile empty

--- src/app/(sippm)/biodata/components/profile.jsx
+++ src/app/(sippm)/biodata/components/profile.jsx
@@ -21,7 +21,7 @@ export const Profile = () => {
           unoptimized
         />
         <p className="mb-1 text-center text-2xl text-[#666666]">
-          {profile?.name}
+          {profile?.name || session?.user.name}
         </p>
       </div>
       <div className="flex flex-col gap-2 px-[18px] py-5">
@@ -30,7 +30,7 @@ export const Profile = () => {
           <div className="flex flex-col gap-2 text-sm font-[500]">
             <p>Role</p>
             <p>Nama</p>
-            <p>NIP</p>
+            <p>NIDN</p>
             <p>Kontak</p>
             <p>Email</p>
           </div>
@@ -42,7 +42,7 @@ export const Profile = () => {
           <div className="flex flex-col gap-2 text-sm font-[500] text-[#666666]">
             <p>{session?.user.roles[0].name}</p>
             <p>{profile?.nama_lengkap}</p>
-            <p>{profile?.nidn_or_nidk_or_nim || "-"}</p>
+            <p>{profile?.nidn_or_nidk_or_nim || session?.user.username}</p>
             <p>{profile?.nomor_hp || "-"}</p>
             <p>{profile?.email}</p>
           </div>
