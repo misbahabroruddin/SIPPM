e0d5fdc72edf45d7628bd4317eda194b28a0ddd5
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-29 15:03:13 +0700

    fix: misspelling typo

--- src/app/(sippm)/proposal/pengabdian/track/[id]/components/detail-list-berkas.jsx
+++ src/app/(sippm)/proposal/pengabdian/track/[id]/components/detail-list-berkas.jsx
@@ -26,10 +26,10 @@ export const DetailListBerkas = ({ data }) => {
         </div>
       </div>
       <div className="flex flex-col gap-2">
-        <h5>File Proposal</h5>
+        <h5>File CV</h5>
         <div className="flex items-center gap-2">
           <Image src="/icons/file.svg" width={24} height={24} alt="File" />
-          <ModalViewerPDF data={data?.data?.file_proposal} />
+          <ModalViewerPDF data={data?.data?.file_cv} />
         </div>
       </div>
     </>
