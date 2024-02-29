42213b87e83e82952f3270ebed2978105f1692ba
misbahabroruddin <misbahabroruddin@gmail.com>
2024-02-29 10:43:04 +0700

    feat: add params page for handler get lsting penelitian lppm

--- src/handlers/lppm/penelitian/query-get-listing-penelitian.jsx
+++ src/handlers/lppm/penelitian/query-get-listing-penelitian.jsx
@@ -4,11 +4,11 @@ import { toast } from "react-toastify";
 
 import { useAxios } from "@/lib/hooks/useAxios";
 
-export const useQueryGetPenelitianLPPM = (search) => {
+export const useQueryGetPenelitianLPPM = (search, page, setPage) => {
   const axios = useAxios();
 
   const query = useQuery({
-    queryKey: ["listingPenelitianLPPM", search],
+    queryKey: ["listingPenelitianLPPM", search, page],
     queryFn: async () => {
       let params;
       if (search) {
@@ -16,10 +16,18 @@ export const useQueryGetPenelitianLPPM = (search) => {
           judul: search,
         };
       }
+
+      if (page) {
+        params = {
+          ...params,
+          page: page,
+        };
+      }
       try {
         const { data } = await axios.get("/proposals/lppms/penelitians", {
           params: params,
         });
+        console.log(data?.data.current_page);
         return data?.data;
       } catch (error) {
         toast.error(error.message);
