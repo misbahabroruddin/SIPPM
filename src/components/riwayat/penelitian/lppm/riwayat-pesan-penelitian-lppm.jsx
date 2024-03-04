df746d3f6ac7b160613c35014ee448a52c009a62
Misbah Abroruddin <misbahabroruddin@staff.unsia.ac.id>
2024-03-04 17:16:43 +0700

    fix: show name at komen chat

--- src/components/riwayat/penelitian/lppm/riwayat-pesan-penelitian-lppm.jsx
+++ src/components/riwayat/penelitian/lppm/riwayat-pesan-penelitian-lppm.jsx
@@ -66,7 +66,7 @@ export const RiwayatPesanPenelitianLPPM = ({ riwayatId, status, catatan }) => {
                 (
                 {riwayat?.user_id === session?.user?.id
                   ? "Saya"
-                  : riwayat?.name || "NAMA REVIEWER"}
+                  : riwayat?.user?.name || "NAMA REVIEWER"}
                 )
               </p>
             </div>
