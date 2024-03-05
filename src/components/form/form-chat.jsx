69181d76387b15f9a7d0e2da48a3e7e1a8af070d
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-05 10:35:10 +0700

    fix: change button submit text

--- src/components/form/form-chat.jsx
+++ src/components/form/form-chat.jsx
@@ -4,18 +4,18 @@ import { ButtonSubmit } from "../button/button-submit";
 
 export const FormChat = ({ onSubmit, register, disabled }) => {
   return (
-    <form className='flex flex-col gap-3 mt-3' onSubmit={onSubmit}>
+    <form className="mt-3 flex flex-col gap-3" onSubmit={onSubmit}>
       <textarea
-        className='w-full border rounded-lg p-2 resize-none focus:outline focus:outline-primary'
-        name='chat'
-        id='chat'
-        cols='30'
-        placeholder='Tulis pesan...'
+        className="w-full resize-none rounded-lg border p-2 focus:outline focus:outline-primary"
+        name="chat"
+        id="chat"
+        cols="30"
+        placeholder="Tulis pesan..."
         {...register("chat", {
           required: true,
         })}
       />
-      <ButtonSubmit disabled={disabled} />
+      <ButtonSubmit disabled={disabled} text="Kirim pesan" />
     </form>
   );
 };
