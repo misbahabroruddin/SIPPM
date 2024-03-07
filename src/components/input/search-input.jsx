696df2644065e293f8d0d6f9abec5584b0b40f8a
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-07 17:31:24 +0700

    style: fix height search input component

--- src/components/input/search-input.jsx
+++ src/components/input/search-input.jsx
@@ -11,7 +11,7 @@ export const SearchInput = ({
 }) => {
   return (
     <div className="w-52 lg:w-64 xl:w-[400px]">
-      <div className="relative h-9 w-full min-w-[200px]">
+      <div className="h-9.5 relative w-full min-w-[200px]">
         <div className="absolute right-3 top-2/4 grid h-5 w-5 -translate-y-2/4 place-items-center text-blue-gray-500">
           <Image src="/icons/search.svg" height={24} width={24} alt="search" />
         </div>
@@ -19,7 +19,7 @@ export const SearchInput = ({
           type="text"
           placeholder={placeholder}
           className={twMerge(
-            "peer h-full w-full rounded-lg border border-blue-gray-200 bg-transparent px-3 py-2.5 !pr-9 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border focus:border-gray-900  focus:outline-0 disabled:cursor-not-allowed disabled:border-0 disabled:bg-blue-gray-50",
+            "peer h-full w-full rounded-lg border border-[#eee] bg-transparent px-3 py-2.5 !pr-9 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-[#eee] placeholder-shown:border-t-[#eee] focus:border focus:border-gray-900  focus:outline-0 disabled:cursor-not-allowed disabled:border-0 disabled:bg-blue-gray-50",
             className,
           )}
           onChange={onChange}
