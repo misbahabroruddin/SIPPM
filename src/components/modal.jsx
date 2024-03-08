846df62a4152e6812c41f899dc38aee9a8e201c4
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-08 16:40:47 +0700

    style: fixing z index

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -6,17 +6,17 @@ export const Modal = ({ open, onClose, children, containerClassName }) => {
   return (
     <div
       onClick={onClose}
-      className={`fixed inset-0 grid place-items-center transition-colors z-[9999] ${
+      className={`fixed inset-0 z-[1000] grid place-items-center transition-colors ${
         open ? "visible bg-black/20 backdrop-blur" : "invisible"
       }`}
     >
       <div
         onClick={(e) => e.stopPropagation()}
         className={twMerge(
-          `bg-white rounded-lg shadow p-6 transition-all w-80 lg:w-[500px] ${
+          `w-80 rounded-lg bg-white p-6 shadow transition-all lg:w-[500px] ${
             open ? "scale-100 opacity-100" : "scale-125 opacity-0"
           }`,
-          containerClassName
+          containerClassName,
         )}
       >
         {children}
