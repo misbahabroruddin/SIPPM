b2fc5dbd22d2f5f9658fa2675499daa929071433
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-08 16:44:00 +0700

    feat: create layout for jabatan fungsional page

---
+++ src/app/(sippm)/data-referensi/jabatan-fungsional/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Jabatan Fungsional",
+  description: "Jabatan Fungsional",
+};
+
+export default function JabatanFungsionalLayout({ children }) {
+  return <>{children}</>;
+}
