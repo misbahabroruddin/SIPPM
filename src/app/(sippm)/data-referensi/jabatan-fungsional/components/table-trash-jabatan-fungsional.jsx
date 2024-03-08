eefb2ca195144594a0cb0761a164a5b5f39418bb
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-08 16:48:22 +0700

    feat: create table trash jabatan fungsional

---
+++ src/app/(sippm)/data-referensi/jabatan-fungsional/components/table-trash-jabatan-fungsional.jsx
@@ -0,0 +1,60 @@
+"use client";
+import { useState } from "react";
+
+import DataTable from "@/components/data-table/table";
+import { SearchInput } from "@/components/input/search-input";
+import { useImportJabatanFungsional } from "@/handlers/data-referensi/jabatan-fungsional/administrator/import-jabatan-fungsional";
+import { useColumnTableTrashJabatanFungsional } from "./column-table";
+import { useDebouncedCallback } from "use-debounce";
+import { useQueryTrashListingJabatanFungsional } from "@/handlers/data-referensi/jabatan-fungsional/administrator/query-get-all-trash-jabatan-fungsional";
+
+export const TableTrashJabatanFungsional = ({ onClose }) => {
+  const [search, setSearch] = useState("");
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+  const columns = useColumnTableTrashJabatanFungsional(onClose);
+  const { mutateAsync: onImportFile } = useImportJabatanFungsional();
+
+  const handleImport = async (e) => {
+    const file = e.target.files[0];
+    await onImportFile(file);
+  };
+
+  const handleSearch = useDebouncedCallback((value) => {
+    setSearch(value);
+  }, 1000);
+
+  const { data, isLoading } = useQueryTrashListingJabatanFungsional(
+    search,
+    pagination.pageIndex + 1,
+  );
+
+  if (isLoading) return <p>Loading.....</p>;
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between">
+        <div className="flex max-w-[252px] gap-4">
+          <SearchInput
+            onChange={(e) => handleSearch(e.target.value)}
+            defaultValue={search}
+          />
+        </div>
+        {/* <div className="flex gap-2">
+          <InputFileImport onChange={handleImport} />
+          <ModalTambahJabatanFungsional />
+        </div> */}
+      </div>
+      <DataTable
+        columns={columns}
+        data={data}
+        total={data?.total}
+        pageCount={data?.last_page}
+        pagination={pagination}
+        setPagination={setPagination}
+      />
+    </div>
+  );
+};
