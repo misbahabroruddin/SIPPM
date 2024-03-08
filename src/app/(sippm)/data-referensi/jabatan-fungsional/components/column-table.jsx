9464b0af32e7b23109026d13816d3588963d3843
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-08 16:46:11 +0700

    feat: add column table hook for table jabatan fungsional

---
+++ src/app/(sippm)/data-referensi/jabatan-fungsional/components/column-table.jsx
@@ -0,0 +1,137 @@
+"use client";
+
+import { useDeleteJabatanFungsional } from "@/handlers/data-referensi/jabatan-fungsional/administrator/delete-jabatan-fungsional";
+import { createColumnHelper } from "@tanstack/react-table";
+import { ModalEditJabatanFungsional } from "./modal-edit-jabatan-fungsional";
+import Image from "next/image";
+import { useRestoreJabatanFungsional } from "@/handlers/data-referensi/jabatan-fungsional/administrator/restore-jabatan-fungsional";
+import { ButtonRestore } from "@/components/button/button-restore";
+import { useDeleteTrashJabatanFungsional } from "@/handlers/data-referensi/jabatan-fungsional/administrator/delete-trash-jabatan-fungsional";
+import Swal from "sweetalert2";
+
+export const useColumnTableJabatanFungsional = () => {
+  const { mutateAsync } = useDeleteJabatanFungsional();
+  const columnHelper = createColumnHelper();
+  const columns = [
+    columnHelper.accessor("id", {
+      header: () => "No",
+      cell: (props) => {
+        return props?.table?.getSortedRowModel()?.rows?.indexOf(props?.row) + 1;
+      },
+      maxSize: 1,
+    }),
+    columnHelper.accessor("nama", {
+      cell: (info) => info.getValue(),
+      header: () => <span>Nama</span>,
+      size: 5,
+    }),
+    columnHelper.accessor("keterangan", {
+      id: "keterangan",
+      cell: (info) => <i>{info.getValue()}</i>,
+      header: () => <span>Keterangan</span>,
+      size: 5,
+    }),
+    columnHelper.accessor("id", {
+      id: "id",
+      header: () => <span>Action</span>,
+      cell: (info) => {
+        return (
+          <div className="flex gap-2">
+            <ModalEditJabatanFungsional id={info.row.original.id} />
+            <button
+              className="rounded-lg disabled:cursor-not-allowed disabled:opacity-60"
+              onClick={async () => {
+                await mutateAsync(info.row.original.id);
+                info.table.reset();
+              }}
+            >
+              <Image
+                src="/icons/delete.svg"
+                width={24}
+                height={24}
+                alt="delete"
+              />
+            </button>
+          </div>
+        );
+      },
+      size: 1,
+    }),
+  ];
+
+  return columns;
+};
+
+export const useColumnTableTrashJabatanFungsional = (onClose) => {
+  const { mutateAsync: deleteTrashJabatanFungsional } =
+    useDeleteTrashJabatanFungsional();
+  const { mutateAsync: restoreJabatanFungsional } =
+    useRestoreJabatanFungsional();
+  const columnHelper = createColumnHelper();
+  const columns = [
+    columnHelper.accessor("id", {
+      header: () => "No",
+      cell: (props) => {
+        return props?.table?.getSortedRowModel()?.rows?.indexOf(props?.row) + 1;
+      },
+      maxSize: 1,
+    }),
+    columnHelper.accessor("nama", {
+      cell: (info) => info.getValue(),
+      header: () => <span>Nama</span>,
+      size: 5,
+    }),
+    columnHelper.accessor("keterangan", {
+      id: "keterangan",
+      cell: (info) => <i>{info.getValue()}</i>,
+      header: () => <span>Keterangan</span>,
+      size: 5,
+    }),
+    columnHelper.accessor("id", {
+      id: "id",
+      header: () => <span>Action</span>,
+      cell: (info) => {
+        return (
+          <div className="flex gap-2">
+            <ButtonRestore
+              onClick={() => restoreJabatanFungsional(info.row.original.id)}
+            />
+            <button
+              className="rounded-lg disabled:cursor-not-allowed disabled:opacity-60"
+              onClick={async () => {
+                Swal.fire({
+                  title: "Anda yakin?",
+                  text: " Anda tidak akan dapat mengembalikan ini!",
+                  icon: "warning",
+                  dangerMode: true,
+                  showCancelButton: true,
+                  confirmButtonText: "Ya, hapus",
+                  cancelButtonText: "Batal",
+                  confirmButtonColor: "#0BD72C",
+                  cancelButtonColor: "#E32626",
+                  reverseButtons: true,
+                }).then((isConfirm) => {
+                  if (isConfirm["isConfirmed"]) {
+                    deleteTrashJabatanFungsional(info.row.original.id);
+                    onClose();
+                  }
+                });
+              }}
+              title="Delete"
+            >
+              <Image
+                src="/icons/delete.svg"
+                width={24}
+                height={24}
+                alt="delete"
+              />
+            </button>
+          </div>
+        );
+      },
+      size: 1,
+    }),
+  ];
+
+  return columns;
+};
