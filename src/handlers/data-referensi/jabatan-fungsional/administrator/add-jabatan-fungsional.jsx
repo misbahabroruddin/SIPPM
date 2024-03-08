b2f6ea1f047cbeb459529b74c532d9432edde812
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-08 16:42:00 +0700

    feat: add handler for create jabatan fungsional for admin

---
+++ src/handlers/data-referensi/jabatan-fungsional/administrator/add-jabatan-fungsional.jsx
@@ -0,0 +1,36 @@
+"use client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+
+export const useCreateListingJabatanFungsional = (onClose) => {
+  const axios = useAxios();
+  const queryClient = useQueryClient();
+
+  const createJabatanFungsional = async (form) => {
+    const formData = new FormData();
+    formData.append("nama", form.nama);
+    if (form.keterangan) formData.append("keterangan", form.keterangan);
+    try {
+      const response = await axios.post(
+        "/data-referensis/jabatan-fungsionals",
+        formData,
+      );
+      queryClient.invalidateQueries({
+        queryKey: ["jabatan-fungsional"],
+      });
+      toast.success(response.data.message);
+      onClose();
+      return response.data;
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const mutate = useMutation({
+    mutationFn: createJabatanFungsional,
+  });
+
+  return { ...mutate };
+};
