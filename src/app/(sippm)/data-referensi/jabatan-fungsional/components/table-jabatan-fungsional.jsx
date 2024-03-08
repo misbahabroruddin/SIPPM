345c5b54e7bde8337a9fe74e713f0302f84f0577
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-08 16:48:18 +0700

    feat: create table jabatan fungsional

---
+++ src/app/(sippm)/data-referensi/jabatan-fungsional/components/table-jabatan-fungsional.jsx
@@ -0,0 +1,64 @@
+"use client";
+import { useState } from "react";
+
+import DataTable from "@/components/data-table/table";
+import { useQueryListingJabatanFungsional } from "@/handlers/data-referensi/jabatan-fungsional/administrator/query-get-all-jabatan-fungsional";
+import { SearchInput } from "@/components/input/search-input";
+import { InputFileImport } from "@/components/input/input-file-import";
+import { ModalTambahJabatanFungsional } from "./modal-tambah-jabatan-fungsional";
+import { useImportJabatanFungsional } from "@/handlers/data-referensi/jabatan-fungsional/administrator/import-jabatan-fungsional";
+import { useColumnTableJabatanFungsional } from "./column-table";
+import { useDebouncedCallback } from "use-debounce";
+import { ModalTrashJabatanFungsional } from "./modal-trash-jabatan-fungsional";
+
+export const TableJabatanFungsional = () => {
+  const [search, setSearch] = useState("");
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+  const columns = useColumnTableJabatanFungsional();
+  const { mutateAsync: onImportFile } = useImportJabatanFungsional();
+
+  const handleImport = async (e) => {
+    const file = e.target.files[0];
+    await onImportFile(file);
+  };
+
+  const handleSearch = useDebouncedCallback((value) => {
+    setSearch(value);
+  }, 1000);
+
+  const { data, isLoading } = useQueryListingJabatanFungsional(
+    search,
+    pagination.pageIndex + 1,
+  );
+
+  if (isLoading) return <p>Loading.....</p>;
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between">
+        <div className="flex w-[512px] gap-4">
+          <SearchInput
+            onChange={(e) => handleSearch(e.target.value)}
+            defaultValue={search}
+          />
+        </div>
+        <div className="flex gap-2">
+          <ModalTrashJabatanFungsional />
+          <InputFileImport onChange={handleImport} />
+          <ModalTambahJabatanFungsional />
+        </div>
+      </div>
+      <DataTable
+        columns={columns}
+        data={data}
+        total={data?.total}
+        pageCount={data?.last_page}
+        pagination={pagination}
+        setPagination={setPagination}
+      />
+    </div>
+  );
+};
