ff2bc045a0f9723426732b1f4856e25cd64f1810
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-08 16:41:48 +0700

    feat: add handler for get detail jabatan fungsional for admin

---
+++ src/handlers/data-referensi/jabatan-fungsional/administrator/get-detail-jabatan-fungsional.jsx
@@ -0,0 +1,22 @@
+"use client";
+import { toast } from "react-toastify";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+import { useQuery } from "@tanstack/react-query";
+
+export const useQueryGetDetailJabatanFungsional = (id) => {
+  const axios = useAxios();
+
+  const query = useQuery({
+    queryKey: ["jabatan-fungsional", id],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `/data-referensis/jabatan-fungsionals/${id}`,
+      );
+      return data.data;
+    },
+    enabled: !!id,
+  });
+
+  return { ...query };
+};
