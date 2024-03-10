427ae602887cba69f3480f11619796ce7e1db9a9
Misbah Abroruddin <misbahabroruddin@staff.unsia.ac.id>
2024-03-10 20:51:42 +0700

    feat: create component table bidang ilmu

---
+++ src/app/(sippm)/data-referensi/bidang-ilmu/components/table-bidang-ilmu.jsx
@@ -0,0 +1,64 @@
+"use client";
+import { useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
+
+import DataTable from "@/components/data-table/table";
+import { SearchInput } from "@/components/input/search-input";
+import { InputFileImport } from "@/components/input/input-file-import";
+import { useColumnTableBidangIlmu } from "./column-table";
+import { useQueryListingBidangIlmu } from "@/handlers/data-referensi/bidang-ilmu/administrator/query-get-all-bidang-ilmu";
+import { useImportBidangIlmu } from "@/handlers/data-referensi/bidang-ilmu/administrator/import-bidang-ilmu";
+import { ModalTambahBidangIlmu } from "./modal-tambah-bidang-ilmu";
+import { ModalTrashBidangIlmu } from "./modal-trash-bidang-ilmu";
+
+export const TableBidangIlmu = () => {
+  const [search, setSearch] = useState("");
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+  const columns = useColumnTableBidangIlmu();
+  const { mutateAsync: onImportFile } = useImportBidangIlmu();
+
+  const handleImport = async (e) => {
+    const file = e.target.files[0];
+    await onImportFile(file);
+  };
+
+  const handleSearch = useDebouncedCallback((value) => {
+    setSearch(value);
+  }, 1000);
+
+  const { data, isLoading } = useQueryListingBidangIlmu(
+    search,
+    pagination.pageIndex + 1,
+  );
+
+  if (isLoading) return <p>Loading.....</p>;
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between">
+        <div className="flex w-[512px] gap-4">
+          <SearchInput
+            onChange={(e) => handleSearch(e.target.value)}
+            defaultValue={search}
+          />
+        </div>
+        <div className="flex gap-2">
+          <ModalTrashBidangIlmu />
+          <InputFileImport onChange={handleImport} />
+          <ModalTambahBidangIlmu />
+        </div>
+      </div>
+      <DataTable
+        columns={columns}
+        data={data}
+        total={data?.total}
+        pageCount={data?.last_page}
+        pagination={pagination}
+        setPagination={setPagination}
+      />
+    </div>
+  );
+};
