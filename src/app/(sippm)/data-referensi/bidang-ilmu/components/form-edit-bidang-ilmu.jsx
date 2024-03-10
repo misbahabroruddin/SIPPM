6b004caaf7a7f188dba6b225cbeff7cbda24e598
Misbah Abroruddin <misbahabroruddin@staff.unsia.ac.id>
2024-03-10 20:49:50 +0700

    feat: create component form edit bidang ilmu

---
+++ src/app/(sippm)/data-referensi/bidang-ilmu/components/form-edit-bidang-ilmu.jsx
@@ -0,0 +1,67 @@
+"use client";
+import { useForm } from "react-hook-form";
+
+import { ButtonCancel } from "@/components/button/button-cancel";
+import { ButtonSave } from "@/components/button/button-save";
+import { Input } from "@/components/input/input";
+import { useQueryEditBidangIlmu } from "@/handlers/data-referensi/bidang-ilmu/administrator/edit-bidang-ilmu";
+
+export const FormEditBidangIlmu = ({ id, setOpen, data, isLoading }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  const { mutateAsync, isPending } = useQueryEditBidangIlmu(id);
+
+  return (
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit(mutateAsync)}>
+      <Input
+        label="Nama"
+        name="nama"
+        containerClass="flex-col items-start gap-1"
+        spanEmptyClass="hidden"
+        placeholder="Nama Jabatan Fungsional"
+        register={register("nama", {
+          required: "Wajib diisi",
+        })}
+        errors={errors.nama}
+        required
+        defaultValue={data?.nama}
+        disabled={isLoading}
+      />
+      <Input
+        label="Keterangan"
+        name="keterangan"
+        containerClass="flex-col items-start gap-1"
+        spanEmptyClass="hidden"
+        placeholder="Keterangan Jabatan Fungsional"
+        register={register("keterangan")}
+        errors={errors.keterangan}
+        defaultValue={data?.keterangan}
+        disabled={isLoading}
+      />
+      <div className="mt-3 flex gap-4">
+        <ButtonCancel
+          className={"w-full"}
+          iconLeft
+          onClick={() => {
+            onClose();
+            reset();
+          }}
+        />
+        <ButtonSave
+          className={"w-full rounded"}
+          iconLeft
+          disabled={isPending}
+        />
+      </div>
+    </form>
+  );
+};
