9d600c207e88a1ebe7b5b50cbec7bc88d6183f7a
Misbah Abroruddin <misbahabroruddin@staff.unsia.ac.id>
2024-03-11 12:51:02 +0700

    fix: optional chaining

--- src/handlers/biodata/query-get-profile.jsx
+++ src/handlers/biodata/query-get-profile.jsx
@@ -11,25 +11,25 @@ export const useGetProfile = (setValue) => {
     try {
       const { data } = await axios.get("/biodata");
       if (setValue) {
-        setValue("nama_lengkap", data?.data.nama_lengkap);
-        setValue("nik", data?.data.nik);
-        setValue("nidn_or_nidk_or_nim", data?.data.nidn_or_nidk_or_nim);
-        setValue("tempat_lahir_id", data?.data.tempat_lahir_id);
-        if (data?.data.tanggal_lahir)
-          setValue("tanggal_lahir", new Date(data?.data.tanggal_lahir));
-        setValue("pangkat_golongan", data?.data.pangkat_golongan);
-        setValue("jabatan_fungsional_id", data?.data.jabatan_fungsional_id, {
+        setValue("nama_lengkap", data?.data?.nama_lengkap);
+        setValue("nik", data?.data?.nik);
+        setValue("nidn_or_nidk_or_nim", data?.data?.nidn_or_nidk_or_nim);
+        setValue("tempat_lahir_id", data?.data?.tempat_lahir_id);
+        if (data?.data?.tanggal_lahir)
+          setValue("tanggal_lahir", new Date(data?.data?.tanggal_lahir));
+        setValue("pangkat_golongan", data?.data?.pangkat_golongan);
+        setValue("jabatan_fungsional_id", data?.data?.jabatan_fungsional_id, {
           shouldValidate: false,
         });
-        setValue("program_studi_id", data?.data.program_studi_id, {
+        setValue("program_studi_id", data?.data?.program_studi_id, {
           shouldValidate: false,
         });
-        setValue("email", data?.data.email);
-        setValue("alamat", data?.data.alamat);
-        setValue("nomor_hp", data?.data.nomor_hp);
-        setValue("sinta_id", data?.data.sinta_id);
-        setValue("scopus_id", data?.data.scopus_id);
-        setValue("google_scholar_id", data?.data.google_scholar_id);
+        setValue("email", data?.data?.email);
+        setValue("alamat", data?.data?.alamat);
+        setValue("nomor_hp", data?.data?.nomor_hp);
+        setValue("sinta_id", data?.data?.sinta_id);
+        setValue("scopus_id", data?.data?.scopus_id);
+        setValue("google_scholar_id", data?.data?.google_scholar_id);
       }
 
       return data?.data;
