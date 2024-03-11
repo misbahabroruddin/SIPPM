c48dcf6d02b87c8843532f371fbe8e7db6809f78
Misbah Abroruddin <misbahabroruddin@staff.unsia.ac.id>
2024-03-11 09:50:46 +0700

    feat: create component trash table prodi

---
+++ src/app/(sippm)/data-referensi/program-studi/components/table-trash-program-studi.jsx
@@ -0,0 +1,50 @@
+"use client";
+import { useState } from "react";
+
+import DataTable from "@/components/data-table/table";
+import { SearchInput } from "@/components/input/search-input";
+import { useColumnTableTrashProgramStudi } from "./column-table";
+import { useDebouncedCallback } from "use-debounce";
+import { SkeletonTableDataRefensi } from "@/components/skeleton/skeleton-table-data-refensi";
+import { useQueryTrashListingProgramStudi } from "@/handlers/data-referensi/program-studi/administrator/query-get-all-trash-program-studi";
+
+export const TableTrashProgramStudi = ({ onClose }) => {
+  const [search, setSearch] = useState("");
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+  const columns = useColumnTableTrashProgramStudi(onClose);
+
+  const handleSearch = useDebouncedCallback((value) => {
+    setSearch(value);
+  }, 1000);
+
+  const { data, isLoading } = useQueryTrashListingProgramStudi(
+    search,
+    pagination.pageIndex + 1,
+  );
+
+  if (isLoading) return <SkeletonTableDataRefensi />;
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between">
+        <div className="flex max-w-[252px] gap-4">
+          <SearchInput
+            onChange={(e) => handleSearch(e.target.value)}
+            defaultValue={search}
+          />
+        </div>
+      </div>
+      <DataTable
+        columns={columns}
+        data={data}
+        total={data?.total}
+        pageCount={data?.last_page}
+        pagination={pagination}
+        setPagination={setPagination}
+      />
+    </div>
+  );
+};
