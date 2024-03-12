8442f5d8c5b7a277b3c35ec439f171f573ce63f8
Misbah Abroruddin <misbahabroruddin@staff.unsia.ac.id>
2024-03-12 10:26:42 +0700

    feat: create component table luaran wajib

---
+++ src/app/(sippm)/data-referensi/luaran-wajib/components/table-luaran-wajib.jsx
@@ -0,0 +1,65 @@
+"use client";
+import { useState } from "react";
+
+import DataTable from "@/components/data-table/table";
+import { SearchInput } from "@/components/input/search-input";
+import { InputFileImport } from "@/components/input/input-file-import";
+import { useColumnTableLuaranWajib } from "./column-table";
+import { useDebouncedCallback } from "use-debounce";
+import { SkeletonTableDataRefensi } from "@/components/skeleton/skeleton-table-data-refensi";
+import { ModalTrashLuaranWajib } from "./modal-trash-luaran-wajib";
+import { ModalTambahLuaranWajib } from "./modal-tambah-luaran-wajib";
+import { useImportLuaranWajib } from "@/handlers/data-referensi/luaran-wajib/administrator/import-luaran-wajib";
+import { useQueryListingLuaranWajib } from "@/handlers/data-referensi/luaran-wajib/administrator/query-get-all-luaran-wajib";
+
+export const TableLuaranWajib = () => {
+  const [search, setSearch] = useState("");
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+  const columns = useColumnTableLuaranWajib();
+  const { mutateAsync: onImportFile } = useImportLuaranWajib();
+
+  const handleImport = async (e) => {
+    const file = e.target.files[0];
+    await onImportFile(file);
+  };
+
+  const handleSearch = useDebouncedCallback((value) => {
+    setSearch(value);
+  }, 1000);
+
+  const { data, isLoading } = useQueryListingLuaranWajib(
+    search,
+    pagination.pageIndex + 1,
+  );
+
+  if (isLoading) return <SkeletonTableDataRefensi />;
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between">
+        <div className="flex gap-4">
+          <SearchInput
+            onChange={(e) => handleSearch(e.target.value)}
+            defaultValue={search}
+          />
+        </div>
+        <div className="flex gap-2">
+          <ModalTrashLuaranWajib />
+          <InputFileImport onChange={handleImport} />
+          <ModalTambahLuaranWajib />
+        </div>
+      </div>
+      <DataTable
+        columns={columns}
+        data={data}
+        total={data?.total}
+        pageCount={data?.last_page}
+        pagination={pagination}
+        setPagination={setPagination}
+      />
+    </div>
+  );
+};
