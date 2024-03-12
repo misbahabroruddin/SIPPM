0cb1a3b81ad79b77444540c919ee6f441ef51563
Misbah Abroruddin <misbahabroruddin@staff.unsia.ac.id>
2024-03-12 09:13:12 +0700

    feat: create handler for get all trash rumpun ilmu

---
+++ src/handlers/data-referensi/rumpun-ilmu/administrator/query-get-all-trash-rumpun-ilmu.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+
+export const useQueryTrashListingRumpunIlmu = (search, page) => {
+  const axios = useAxios();
+  const params = {
+    nama: search,
+    page: page,
+  };
+
+  const query = useQuery({
+    queryKey: ["trash-rumpun-ilmu", search, page],
+    queryFn: async () => {
+      try {
+        const { data } = await axios.get(
+          "/data-referensis/rumpun-ilmus/trashs",
+          { params },
+        );
+
+        return data.data;
+      } catch (error) {
+        toast.error(error.message);
+      }
+    },
+  });
+
+  return { ...query };
+};
