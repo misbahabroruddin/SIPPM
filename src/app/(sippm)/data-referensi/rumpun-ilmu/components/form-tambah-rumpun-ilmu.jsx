1bdc3f6d2b42925af0e8daaa587ce59a4ed12911
Misbah Abroruddin <misbahabroruddin@staff.unsia.ac.id>
2024-03-12 09:09:16 +0700

    feat: create form for add rumpun ilmu

---
+++ src/app/(sippm)/data-referensi/rumpun-ilmu/components/form-tambah-rumpun-ilmu.jsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+
+import { ButtonCancel } from "@/components/button/button-cancel";
+import { ButtonSave } from "@/components/button/button-save";
+import { Input } from "@/components/input/input";
+import { useCreateRumpunIlmu } from "@/handlers/data-referensi/rumpun-ilmu/administrator/add-rumpun-ilmu";
+
+export const FormTambahRumpunIlmu = ({ setOpen }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  const { mutateAsync, isPending } = useCreateRumpunIlmu(onClose, reset);
+
+  return (
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit(mutateAsync)}>
+      <Input
+        label="Nama"
+        name="nama"
+        containerClass="flex-col items-start gap-1"
+        spanEmptyClass="hidden"
+        placeholder="Nama Jenis Penelitian"
+        register={register("nama", {
+          required: "Wajib diisi",
+        })}
+        errors={errors.nama}
+        required
+      />
+      <Input
+        label="Keterangan"
+        name="keterangan"
+        containerClass="flex-col items-start gap-1"
+        spanEmptyClass="hidden"
+        placeholder="Keterangan Jenis Penelitian"
+        register={register("keterangan")}
+        errors={errors.keterangan}
+      />
+      <div className="mt-3 flex gap-4">
+        <ButtonCancel
+          className={"w-full"}
+          iconLeft
+          onClick={() => {
+            onClose();
+            reset();
+          }}
+        />
+        <ButtonSave
+          className={"w-full rounded"}
+          iconLeft
+          disabled={isPending}
+        />
+      </div>
+    </form>
+  );
+};
