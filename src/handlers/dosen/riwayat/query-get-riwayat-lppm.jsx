9bfcc6e98f729582902869a1d3908dcc9ca8e8b9
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-16 15:07:57 +0700

    fix: breaking change BE API

---
+++ src/handlers/dosen/riwayat/query-get-riwayat-lppm.jsx
@@ -0,0 +1,34 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { notFound, useParams } from "next/navigation";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+
+export const useQueryGetRiwayatTrackDosenLPPM = () => {
+  const axios = useAxios();
+  const { id } = useParams();
+
+  const { data, status, isLoading, refetch } = useQuery({
+    queryKey: ["trackDosenLPPM", id],
+    queryFn: async () => {
+      try {
+        const { data } = await axios.get(
+          `/proposals/dosens/verifikasis/${id}/lppms`,
+        );
+        return data.data;
+      } catch (error) {
+        toast.error(error.message);
+      }
+    },
+  });
+
+  // if (status === "error") return notFound();
+
+  return {
+    data,
+    isLoading,
+    status,
+    refetch,
+  };
+};
