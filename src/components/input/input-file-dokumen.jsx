d886db993cf45971a585385204cab87fe5941fba
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-16 22:52:38 +0700

    fix: make default input file

--- src/components/input/input-file-dokumen.jsx
+++ src/components/input/input-file-dokumen.jsx
@@ -4,22 +4,12 @@ import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 export const InputFileDokumen = ({
-  watch,
   register,
   name,
   errors,
   label,
   accept = "application/pdf",
 }) => {
-  const [preview, setPreview] = useState();
-  useEffect(() => {
-    const file = watch(name);
-    if (file) {
-      setPreview(file[0]?.name);
-    } else {
-      setPreview(null);
-    }
-  }, [watch(name)]);
   return (
     <div className="flex items-center">
       <label
@@ -31,21 +21,7 @@ export const InputFileDokumen = ({
       >
         {label}
       </label>
-      <label
-        htmlFor={name}
-        className="flex grow cursor-pointer rounded border border-gray-200 p-2 text-center "
-      >
-        {preview ? preview : "Pilih File"}
-        {errors && <p className="mt-2 text-red-500">{errors.message}</p>}
-      </label>
-      <input
-        type="file"
-        name={name}
-        id={name}
-        className="hidden"
-        {...register}
-        accept={accept}
-      />
+      <input type="file" name={name} id={name} {...register} accept={accept} />
     </div>
   );
 };
