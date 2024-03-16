8c489f03a7bcfc5ba28403ab68c2d7d35206c397
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-16 21:28:01 +0700

    fix: delete unnecessary variable

--- src/app/(sippm)/document/penelitian/components/lppm/list-document-kontrak-lppm.jsx
+++ src/app/(sippm)/document/penelitian/components/lppm/list-document-kontrak-lppm.jsx
@@ -1,14 +1,9 @@
 "use client";
-import Image from "next/image";
 
 import { EmptyState } from "@/components/empty-state";
 import { SkeletonListingProposal } from "@/components/skeleton/skeleton-listing-proposal";
 import { Pagination } from "@/components/pagination";
-import { ButtonDownload } from "@/components/button/button-download";
-import { convertDate } from "@/lib/utils/convertDate";
-import { ListItemKontrakDibalas } from "./list-item-kontrak-dibalas";
 import { ListItemKontrakMenunggu } from "./list-item-kontrak-menunggu";
-import { useId } from "react";
 
 export const ListPenelitianKontrakLppm = ({
   penelitian,
@@ -16,7 +11,6 @@ export const ListPenelitianKontrakLppm = ({
   handlePageChange,
 }) => {
   if (isLoading) return <SkeletonListingProposal />;
-  const id = useId();
   return (
     <div className="flex flex-col gap-4">
       <div className="flex h-[580px] flex-col gap-4 overflow-auto p-1">
