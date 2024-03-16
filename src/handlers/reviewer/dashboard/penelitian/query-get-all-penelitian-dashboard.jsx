7aab7c42a9ed803c7185b2129b2cf2109d77274a
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-16 21:09:44 +0700

    feat: create handler for get list penelitian dashboard reviewer

---
+++ src/handlers/reviewer/dashboard/penelitian/query-get-all-penelitian-dashboard.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+
+import { useAxios } from "@/lib/hooks/useAxios";
+
+export const useQueryGetAllPenelitianDashboardReviewer = (search, page) => {
+  const axios = useAxios();
+  const query = useQuery({
+    queryKey: ["listPenelitianDashboardReviewer", search, page],
+    queryFn: async () => {
+      let params;
+      if (search) {
+        params = {
+          judul: search,
+        };
+      }
+
+      if (page) {
+        params = {
+          ...params,
+          page: page,
+        };
+      }
+      try {
+        const { data } = await axios.get(`/dashboards/reviewers/penelitians`, {
+          params: params,
+        });
+
+        return data.data;
+      } catch (error) {
+        toast.error(error.message);
+      }
+    },
+  });
+  return { ...query };
+};
