b8e69289bb01019e1c4c85c629aa48818925e73e
Misbah Abroruddin <misbahabroruddin@staff.unsia.ac.id>
2024-03-17 10:09:23 +0700

    feat: add filter for status kontrak diterima

--- src/app/(sippm)/document/penelitian/components/lppm/list-document-kontrak-lppm.jsx
+++ src/app/(sippm)/document/penelitian/components/lppm/list-document-kontrak-lppm.jsx
@@ -1,14 +1,15 @@
 "use client";
 import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 import { EmptyState } from "@/components/empty-state";
 import { SkeletonListingProposal } from "@/components/skeleton/skeleton-listing-proposal";
 import { Pagination } from "@/components/pagination";
 
-import { twMerge } from "tailwind-merge";
 import { ListItemKontrakDibalas } from "./list-item-kontrak-dibalas";
 import { ListItemKontrakDikirim } from "./list-item-dikirim";
 import { ListItemKontrakMenunggu } from "./list-item-kontrak-menunggu";
+import { ListItemKontrakDiterima } from "./list-item-kontrak-diterima";
 
 export const ListPenelitianKontrakLppm = ({
   penelitian,
@@ -30,13 +31,17 @@ export const ListPenelitianKontrakLppm = ({
     (proposal) => proposal?.dokumen_kontrak?.status === "Dibalas",
   );
 
+  const kontrakDiterima = penelitian?.data?.filter(
+    (proposal) => proposal?.dokumen_kontrak?.status === "Diterima",
+  );
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-4">
         <div
           className={twMerge(
-            "flex h-[120px] w-[200px] flex-col items-center justify-center gap-6 rounded-lg border border-sky-05 p-3 font-[500] text-sky-05",
-            tab === "Menunggu" ? "bg-sky-05 text-white" : null,
+            "border-red-07 text-red-07 flex h-[120px] w-[200px] flex-col items-center justify-center gap-6 rounded-lg border p-3 font-[500]",
+            tab === "Menunggu" ? "bg-red-07 text-white" : null,
           )}
           role="button"
           onClick={() => setTab("Menunggu")}
@@ -46,7 +51,7 @@ export const ListPenelitianKontrakLppm = ({
         </div>
         <div
           className={twMerge(
-            "text-green-05 border-green-05 flex h-[120px] w-[200px] flex-col items-center justify-center gap-6 rounded-lg border p-3 font-[500]",
+            "flex h-[120px] w-[200px] flex-col items-center justify-center gap-6 rounded-lg border border-green-05 p-3 font-[500] text-green-05",
             tab === "Dikirim" ? "bg-green-05 text-white" : null,
           )}
           role="button"
@@ -57,14 +62,25 @@ export const ListPenelitianKontrakLppm = ({
         </div>
         <div
           className={twMerge(
-            "flex h-[120px] w-[200px] flex-col items-center justify-center gap-6 rounded-lg border border-blue-primary p-3 font-[500] text-blue-primary",
-            tab === "Dibalas" ? "bg-blue-primary text-white" : null,
+            "flex h-[120px] w-[200px] flex-col items-center justify-center gap-6 rounded-lg border border-sky-05 p-3 font-[500] text-sky-05",
+            tab === "Dibalas" ? "bg-sky-05 text-white" : null,
           )}
           role="button"
           onClick={() => setTab("Dibalas")}
         >
           <p className="text-3xl">{kontrakDibalas?.length}</p>
           <p className="text-xl">Dibalas</p>
+        </div>
+        <div
+          className={twMerge(
+            "flex h-[120px] w-[200px] flex-col items-center justify-center gap-6 rounded-lg border border-blue-primary p-3 font-[500] text-blue-primary",
+            tab === "Diterima" ? "bg-blue-primary text-white" : null,
+          )}
+          role="button"
+          onClick={() => setTab("Diterima")}
+        >
+          <p className="text-3xl">{kontrakDiterima?.length}</p>
+          <p className="text-xl">Diterima</p>
         </div>
       </div>
       <div className="flex h-[480px] flex-col gap-4 overflow-auto p-1">
@@ -90,6 +106,15 @@ export const ListPenelitianKontrakLppm = ({
           kontrakDibalas?.length ? (
             kontrakDibalas?.map((proposal) => (
               <ListItemKontrakDibalas key={proposal.id} data={proposal} />
+            ))
+          ) : (
+            <EmptyState />
+          )
+        ) : null}
+        {tab === "Diterima" ? (
+          kontrakDiterima?.length ? (
+            kontrakDiterima?.map((proposal) => (
+              <ListItemKontrakDiterima key={proposal.id} data={proposal} />
             ))
           ) : (
             <EmptyState />
