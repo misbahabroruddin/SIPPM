aafd97f49905b89e208cf14c98c360fbbac92468
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-18 13:10:00 +0700

    fix: limit kabupaten

--- src/handlers/data-referensi/kabupaten/query-search-kabupaten.jsx
+++ src/handlers/data-referensi/kabupaten/query-search-kabupaten.jsx
@@ -9,7 +9,9 @@ export const useQuerySearchKabupaten = () => {
   const axios = useAxios();
   const fetchSearchKabupaten = async () => {
     try {
-      const { data } = await axios.get("/data-referensis/kabupatens/search");
+      const { data } = await axios.get(
+        "/data-referensis/kabupatens/search?limit=1000",
+      );
       const result = data.data.map((opt) => ({
         value: opt.id,
         label: opt.nama,
