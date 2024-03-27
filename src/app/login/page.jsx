cd88acf5aa4788e4ba17b3395fca0ffc0de58cc9
misbahabroruddin <misbahabroruddin@gmail.com>
2024-03-27 12:11:39 +0700

    fix: callback url

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -6,9 +6,9 @@ import { useEffect } from "react";
 export default function Login() {
   useEffect(() => {
     signIn("sso", {
-      callbackUrl: `${window.location.href}/dashboard`,
+      callbackUrl: `/dashboard`,
     });
-  }, []);
+  }, [signIn]);
   return (
     <div className="relative flex h-screen items-center justify-center font-poppins">
       <div className="h-10 w-10 animate-spin rounded-full border-2 border-gray-300 border-t-primary" />
