a5865b284f57b6e32a6f517a975cf5aaf81c7a93
misbahabroruddin <misbahabroruddin@gmail.com>
2024-04-23 16:44:00 +0700

    feat: responsive list sk kontrak dosen

--- src/app/(sippm)/document/penelitian/components/dosen/list-document-sk-dosen.jsx
+++ src/app/(sippm)/document/penelitian/components/dosen/list-document-sk-dosen.jsx
@@ -16,7 +16,7 @@ export const ListPenelitianSKDosen = ({
   if (isLoading) return <SkeletonListingProposal />;
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex h-[580px] flex-col gap-4 overflow-auto p-1">
+      <div className="flex h-[380px] flex-col gap-4 overflow-auto p-1 md:h-[450px] lg:h-[580px]">
         {penelitian?.data?.length ? (
           penelitian?.data?.map((proposal) => (
             <ListItemSK key={proposal.id} data={proposal} />
@@ -40,9 +40,9 @@ export const ListPenelitianSKDosen = ({
 const ListItemSK = ({ data }) => {
   return (
     <div className="rounded-lg px-6 py-4 shadow-custom" key={data?.id}>
-      <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between">
+      <div className="flex flex-col gap-3 lg:flex-row lg:items-center lg:justify-between">
         <div className="flex w-fit flex-col gap-1 lg:max-w-[731px]">
-          <h2 className="text-lg">{data?.judul}</h2>
+          <h2 className="text-base lg:text-lg">{data?.judul}</h2>
           <div className="flex flex-col gap-2">
             <div className="flex flex-wrap gap-x-4 gap-y-2">
               <div className="flex items-center gap-[2px]">
@@ -51,8 +51,11 @@ const ListItemSK = ({ data }) => {
                   height={24}
                   width={24}
                   alt="author"
+                  className="h-5 w-5"
                 />
-                <p className="text-sm text-[#999999]">{data?.user?.name}</p>
+                <p className="text-xs text-[#999999] md:text-sm">
+                  {data?.user?.name}
+                </p>
               </div>
               <div className="flex items-center gap-[2px]">
                 <Image
@@ -60,8 +63,9 @@ const ListItemSK = ({ data }) => {
                   height={24}
                   width={24}
                   alt="tanggal"
+                  className="h-5 w-5"
                 />
-                <p className="text-sm text-[#999999]">
+                <p className="text-xs text-[#999999] md:text-sm">
                   {convertDate(data?.created_at)}
                 </p>
               </div>
@@ -72,38 +76,29 @@ const ListItemSK = ({ data }) => {
                   src="/icons/Book.svg"
                   height={24}
                   width={24}
-                  alt="mata kuliah"
+                  alt="Program studi"
+                  className="h-5 w-5"
                 />
-                <p className="text-sm text-[#999999]">
+                <p className="text-xs text-[#999999] md:text-sm">
                   {data?.user?.biodata?.program_studi?.nama}
                 </p>
               </div>
-              {/* <div className="flex items-center gap-[2px]">
-                <Image
-                  src="/icons/date-edit.svg"
-                  height={24}
-                  width={24}
-                  alt="tanggal"
-                />
-                <p className="text-sm text-[#999999]">{data?.tahun_ajaran}</p>
-              </div> */}
             </div>
           </div>
         </div>
         <div className="flex items-end gap-4">
           {data?.dokumen_sk ? (
             <Link
-              // onClick={handleSaveFile}
               href={data?.dokumen_sk?.file_sk?.url}
               target="_blank"
-              className="flex items-center gap-2 rounded-lg bg-primary text-white disabled:cursor-not-allowed disabled:bg-gray-500"
+              className="flex w-full items-center justify-center gap-2 rounded-lg bg-primary text-white disabled:cursor-not-allowed disabled:bg-gray-500 md:w-fit"
             >
-              <ButtonDownload />
+              <ButtonDownload className="px-2 py-1 md:px-4" />
             </Link>
           ) : (
-            <div className="flex flex-col items-center gap-[2px] pr-2 font-[500]">
-              <p className="text-sm text-primary">Status</p>
-              <p className="rounded-lg bg-[#CCCCCC] px-2 py-1 text-[#333333]">
+            <div className="flex w-full flex-col items-center gap-[2px] pr-2 font-[500] md:w-fit">
+              <p className="text-xs text-primary lg:text-sm">Status</p>
+              <p className="text-[#333333 rounded-lg bg-[#CCCCCC] px-2 py-1 text-sm lg:text-base">
                 SK Belum Ada
               </p>
             </div>
