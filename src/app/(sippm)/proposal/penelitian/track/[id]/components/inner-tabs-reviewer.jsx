15d9d76e7de609aaeae986d6f48be4cbc7d797a1
misbahabroruddin <misbahabroruddin@gmail.com>
2024-05-21 01:06:50 +0700

    feat: create component inner tab for reviewer

---
+++ src/app/(sippm)/proposal/penelitian/track/[id]/components/inner-tabs-reviewer.jsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+
+import { Tab } from "@/components/tab";
+
+export const InnerTabsReviewer = ({ tabActive }) => {
+  const pathname = usePathname();
+  const tabParams = useSearchParams();
+  const router = useRouter();
+
+  const tabs = [
+    {
+      id: 1,
+      name: "Identitas Usulan",
+      func: () => handleTabClick("Identitas Usulan"),
+    },
+    {
+      id: 2,
+      name: "anggota",
+      func: () => handleTabClick("anggota"),
+    },
+    {
+      id: 3,
+      name: "Luaran dan Target Capaian",
+      func: () => handleTabClick("Luaran dan Target Capaian"),
+    },
+    {
+      id: 4,
+      name: "Rencana Anggaran",
+      func: () => handleTabClick("Rencana Anggaran"),
+    },
+    {
+      id: 5,
+      name: "Jadwal",
+      func: () => handleTabClick("Jadwal"),
+    },
+    {
+      id: 6,
+      name: "Berkas",
+      func: () => handleTabClick("Berkas"),
+    },
+    {
+      id: 7,
+      name: "Penilaian",
+      func: () => handleTabClick("Penilaian"),
+    },
+  ];
+
+  const createQueryTab = useCallback(
+    (name, value) => {
+      const params = new URLSearchParams(tabParams.toString());
+      params.set(name, value);
+      return params.toString();
+    },
+    [tabParams],
+  );
+
+  const handleTabClick = (tabName) => {
+    router.push(`${pathname}?${createQueryTab("tab2", tabName)}`);
+  };
+
+  return (
+    <div className="flex flex-wrap gap-2">
+      {tabs.map(({ id, name, icon, func }) => {
+        return (
+          <Tab
+            key={id}
+            tabName={name}
+            tabActive={tabActive}
+            onClick={func}
+            className="rounded rounded-bl-none rounded-br-none border-none bg-[#10487A] text-sm text-white outline-none lg:w-fit lg:text-base"
+            tabActiveClass="rounded rounded-bl-none rounded-br-none border border-b-0 border-blue-primary bg-white text-black w-fit lg:w-fit text-sm lg:text-base"
+          />
+        );
+      })}
+    </div>
+  );
+};
