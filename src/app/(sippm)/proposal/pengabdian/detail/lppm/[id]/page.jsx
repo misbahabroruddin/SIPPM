f6410e8d1b8fcfa0f6b88b748f6323845e4319b9
misbahabroruddin <misbahabroruddin@gmail.com>
2024-06-11 11:38:27 +0700

    feat: create detail pengabdian page for lppm

---
+++ src/app/(sippm)/proposal/pengabdian/detail/lppm/[id]/page.jsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+import { BasePageTitle } from "@/components/base-page-title";
+import { ContainerPage } from "@/components/container-page";
+import { InnerTabsLppm } from "@/components/proposal/track/inner-tabs-lppm";
+import { SectionHeaderDetailProposal } from "@/components/proposal/track/section-header-detail-proposal";
+import { Tabs } from "@/components/proposal/track/tabs";
+import { useQueryDetailPengabdian } from "@/handlers/lppm/pengabdian/query-get-detail-pengabdian";
+import { useQueryGetRiwayatVerikasiLPPM } from "@/handlers/lppm/riwayat/query-get-verifikasi-proposal-lppm";
+import { DetailIdentitasUsulanPengabdian } from "../../components/detail-identitas-usulan-pengabdian";
+import { DetailAnggota } from "@/components/proposal/track/detail-anggota";
+import { DetailTargetCapaian } from "@/components/proposal/track/track-detail-target-capaian";
+import { DetailRencanaAnggaran } from "@/components/proposal/track/detail-rencana-anggaran";
+import { DetailRincianKegiatan } from "@/components/proposal/track/detail-rincian-kegiatan";
+import { DetailBerkas } from "@/components/proposal/track/detail-berkas";
+import { Timeline } from "@material-tailwind/react";
+import { TrackRiwayatLPPM } from "@/components/riwayat/lppm/riwayat-lppm";
+
+export default function DetailPengabdianLPPMPage() {
+  const [tabActive] = useState("dokumen");
+  const [innerTabActive] = useState("Identitas Usulan");
+  const tabParams = useSearchParams();
+  const currentTab = tabParams.get("tab");
+  const innerTab = tabParams.get("tab2");
+
+  const { data } = useQueryDetailPengabdian();
+  const { data: dataTrackDosenLPPM, isLoading: isLoadingTrackDosenLPPM } =
+    useQueryGetRiwayatVerikasiLPPM();
+
+  return (
+    <ContainerPage>
+      <div className="flex flex-col gap-3">
+        <BasePageTitle iconSrc="/icons/search-black.svg" title={"Pengabdian"} />
+        <SectionHeaderDetailProposal data={data?.data} />
+        <Tabs tabActive={currentTab || tabActive} />
+        <div className="custom mb-14 flex flex-col gap-3 rounded-lg p-4 shadow-custom">
+          {currentTab === "dokumen" || !currentTab ? (
+            <>
+              <InnerTabsLppm tabActive={innerTab || innerTabActive} />
+              <div className="flex flex-col gap-4">
+                {(innerTab === "Identitas Usulan" || !innerTab) && (
+                  <DetailIdentitasUsulanPengabdian data={data} />
+                )}
+                {innerTab === "anggota" && <DetailAnggota data={data} />}
+                {innerTab === "Luaran dan Target Capaian" && (
+                  <DetailTargetCapaian data={data} />
+                )}
+                {innerTab === "Rencana Anggaran" && (
+                  <DetailRencanaAnggaran data={data} />
+                )}
+                {innerTab === "Jadwal" && <DetailRincianKegiatan data={data} />}
+                {innerTab === "Berkas" && <DetailBerkas data={data} />}
+              </div>
+            </>
+          ) : (
+            <Timeline>
+              <TrackRiwayatLPPM
+                data={dataTrackDosenLPPM}
+                isLoading={isLoadingTrackDosenLPPM}
+              />
+            </Timeline>
+          )}
+        </div>
+      </div>
+    </ContainerPage>
+  );
+}
