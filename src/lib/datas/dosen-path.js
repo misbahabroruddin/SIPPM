81bb5b06f163b704ca9064fb2d346e0d03a03d9f
misbahabroruddin <misbahabroruddin@gmail.com>
2024-06-11 11:37:36 +0700

    feat: add path for dosen

--- src/lib/datas/dosen-path.js
+++ src/lib/datas/dosen-path.js
@@ -1,6 +1,27 @@
-export const DOSEN_PATHS = [
+const DOSEN_PATHS = [
   "/proposal/penelitian/tambah",
-  "/proposal/penelitian/edit/*",
-  "/proposal/penelitian/track/*",
+  /^\/proposal\/penelitian\/edit\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
+  /^\/proposal\/pengabdian\/edit\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
+  /^\/proposal\/penelitian\/track\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
+  /^\/proposal\/pengabdian\/track\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
   "/laporan-hasil",
 ];
+
+export function matchesDosenPath(path) {
+  for (const route of DOSEN_PATHS) {
+    if (typeof route === "string") {
+      if (route.endsWith("/*")) {
+        if (path.startsWith(route.slice(0, -1))) {
+          return true;
+        }
+      } else if (route === path) {
+        return true;
+      }
+    } else if (route instanceof RegExp) {
+      if (route.test(path)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
