d9d65d6d8fc229ef5b1e4651ad613ff4ab7dedf9
misbahabroruddin <misbahabroruddin@gmail.com>
2024-06-11 11:40:40 +0700

    fix: only dosen path for track proposal

--- src/app/(sippm)/proposal/pengabdian/track/[id]/page.jsx
+++ src/app/(sippm)/proposal/pengabdian/track/[id]/page.jsx
@@ -3,16 +3,9 @@ import { getServerSession } from "next-auth";
 
 import { ContainerPage } from "@/components/container-page";
 import { authOptions } from "@/config/auth";
-import { DOSEN, LPPM, REVIEWER } from "@/lib/constants/role";
-import TrackPengabdianReviewerPage from "./components/views/track-pengabdian-reviewer";
 
 const TrackPengabdianDosenPage = dynamic(
   () => import("./components/views/track-pengabdian-dosen"),
-  { ssr: false },
-);
-
-const TrackPengabdianLPPMPage = dynamic(
-  () => import("./components/views/track-pengabdian-lppm"),
   { ssr: false },
 );
 
@@ -22,9 +15,7 @@ export default async function PageTrackProposal() {
 
   return (
     <ContainerPage>
-      {role === DOSEN && <TrackPengabdianDosenPage />}
-      {role === LPPM && <TrackPengabdianLPPMPage />}
-      {role === REVIEWER && <TrackPengabdianReviewerPage />}
+      <TrackPengabdianDosenPage />
     </ContainerPage>
   );
 }
