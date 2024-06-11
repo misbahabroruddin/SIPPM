de3f665fdf2f82e21f645e799a555b3caa898827
misbahabroruddin <misbahabroruddin@gmail.com>
2024-06-11 16:06:59 +0700

    fix: fixing condition when biodata is null

--- src/components/proposal/track/section-header-detail-proposal.jsx
+++ src/components/proposal/track/section-header-detail-proposal.jsx
@@ -5,7 +5,9 @@ import { convertDate } from "@/lib/utils/convertDate";
 export const SectionHeaderDetailProposal = ({ data }) => {
   return (
     <div className="flex flex-col gap-2">
-      <h1 className="text-lg font-[500] lg:text-xl">{data?.judul}</h1>
+      <h1 className="text-lg font-[500] lg:text-xl">
+        {data?.judul || "Judul Proposal"}
+      </h1>
       <div className="flex w-full flex-wrap justify-between text-black-07 lg:w-3/4 lg:flex-nowrap">
         <div className="flex w-full justify-between lg:w-2/4">
           <div className="flex w-[180px] flex-col justify-start gap-2 text-sm lg:w-full lg:justify-between lg:text-base">
@@ -16,7 +18,7 @@ export const SectionHeaderDetailProposal = ({ data }) => {
                 <p>
                   {data?.user?.biodata
                     ? data?.user?.biodata?.nama_lengkap
-                    : "-"}
+                    : data?.user?.name}
                 </p>
               </div>
             </div>
@@ -67,14 +69,18 @@ export const SectionHeaderDetailProposal = ({ data }) => {
                 <p>{data?.user?.biodata?.program_studi?.nama || "-"}</p>
               </div>
             </div>
-            <div className="flex gap-2">
+            <div className="flex gap-2 lg:hidden">
               <p className="block min-w-[120px] lg:hidden">Email</p>
               <div className="flex min-w-[200px] gap-2 lg:hidden lg:gap-4">
                 <span>:</span>
-                <p>{data?.user?.biodata ? data?.user?.biodata?.email : "-"}</p>
+                <p>
+                  {data?.user?.biodata
+                    ? data?.user?.biodata?.email
+                    : data?.user?.email}
+                </p>
               </div>
             </div>
-            <div className="flex gap-2">
+            <div className="flex gap-2 lg:hidden">
               <p className="block min-w-[120px] lg:hidden">Nomor Hp</p>
               <div className="flex min-w-[200px] gap-2 lg:hidden lg:gap-4">
                 <span>:</span>
@@ -83,23 +89,23 @@ export const SectionHeaderDetailProposal = ({ data }) => {
                 </p>
               </div>
             </div>
-            <div className="flex gap-2">
-              <p className="block min-w-[120px] lg:hidden">ID Sinta</p>
-              <div className="flex min-w-[200px] gap-2 lg:hidden lg:gap-4">
+            <div className="flex gap-2 lg:hidden">
+              <p className="block min-w-[120px]">ID Sinta</p>
+              <div className="flex min-w-[200px] gap-2 lg:gap-4">
                 <span>:</span>
                 <p>{data?.user?.biodata?.sinta_id || "-"}</p>
               </div>
             </div>
-            <div className="flex gap-2">
-              <p className="block min-w-[120px] lg:hidden">ID Scopus</p>
-              <div className="flex min-w-[200px] gap-2 lg:hidden lg:gap-4">
+            <div className="flex gap-2 lg:hidden">
+              <p className="block min-w-[120px]">ID Scopus</p>
+              <div className="flex min-w-[200px] gap-2 lg:gap-4">
                 <span>:</span>
                 <p>{data?.user?.biodata?.scopus_id || "-"}</p>
               </div>
             </div>
-            <div className="flex gap-2">
-              <p className="block min-w-[120px] lg:hidden">ID Google Scholar</p>
-              <div className="flex min-w-[200px] gap-2 lg:hidden lg:gap-4">
+            <div className="flex gap-2 lg:hidden">
+              <p className="block min-w-[120px]">ID Google Scholar</p>
+              <div className="flex min-w-[200px] gap-2 lg:gap-4">
                 <span>:</span>
                 <p>{data?.user?.biodata?.google_scholar_id || "-"}</p>
               </div>
@@ -113,7 +119,11 @@ export const SectionHeaderDetailProposal = ({ data }) => {
                 <p className="min-w-[120px] lg:min-w-[155px]">Email</p>
                 <div className="flex gap-2 lg:gap-4">
                   <span>:</span>
-                  <p>{data?.user?.biodata?.email || "-"}</p>
+                  <p>
+                    {data?.user?.biodata
+                      ? data?.user?.biodata?.email
+                      : data?.user?.email}
+                  </p>
                 </div>
               </div>
               <div className="flex gap-2 lg:gap-4">
