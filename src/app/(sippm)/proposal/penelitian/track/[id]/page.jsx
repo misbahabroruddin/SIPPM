e42b888055d87e135f1411e9eaff9e0034e15f6e
misbahabroruddin <misbahabroruddin@gmail.com>
2024-06-12 10:38:50 +0700

    fix: track penelitian for dosen

--- src/app/(sippm)/proposal/penelitian/track/[id]/page.jsx
+++ src/app/(sippm)/proposal/penelitian/track/[id]/page.jsx
@@ -3,32 +3,18 @@ import dynamic from "next/dynamic";
 
 import { ContainerPage } from "@/components/container-page";
 import { authOptions } from "@/config/auth";
-import { DOSEN, LPPM, REVIEWER } from "@/lib/constants/role";
 
 const TrackPenelitianDosenPage = dynamic(
   () => import("./components/views/track-penelitian-dosen"),
   { ssr: false },
 );
 
-const TrackPenelitianLPPMPage = dynamic(
-  () => import("./components/views/track-penelitian-lppm"),
-  { ssr: false },
-);
-
-const TrackPenelitianReviewerPage = dynamic(
-  () => import("./components/views/track-penelitian-reviewer"),
-  { ssr: false },
-);
-
 export default async function TrackPenelitianPage() {
   const session = await getServerSession(authOptions);
-  const role = session?.user?.roles[0]?.name;
 
   return (
     <ContainerPage>
-      {role === DOSEN && <TrackPenelitianDosenPage />}
-      {role === LPPM && <TrackPenelitianLPPMPage />}
-      {role === REVIEWER && <TrackPenelitianReviewerPage />}
+      <TrackPenelitianDosenPage />
     </ContainerPage>
   );
 }
