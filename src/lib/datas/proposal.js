04e285a87c3051f2c47fafa632c8a5052de57ec0
misbahabroruddin <misbahabroruddin@gmail.com>
2024-07-23 21:10:06 +0700

    add new constant

---
+++ src/lib/datas/proposal.js
@@ -0,0 +1,14 @@
+export const STATUS_CAPAIAN_OPTIONS = [
+  {
+    label: "Telah Terbit",
+    value: "Telah Terbit",
+  },
+  {
+    label: "Sedang ditinjau",
+    value: "Sedang ditinjau",
+  },
+  {
+    label: "Ditolak",
+    value: "Ditolak",
+  },
+];
